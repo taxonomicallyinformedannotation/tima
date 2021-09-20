78d2a8decb4ce37ea59204799095424ee927f9bd
Adriano Rutz <adriano.rutz@hotmail.com>
2021-09-20 21:44:13 +0000

    Update integration_test.sh

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -7,15 +7,15 @@ if [ ! -d config ]; then
 fi
 
 bash src/get_lotus.sh &&
-Rscript src/prepare_lotus.R &&
-Rscript src/prepare_library.R &&
-Rscript src/prepare_adducts.R &&
+conda run -n tima Rscript src/prepare_lotus.R &&
+conda run -n tima Rscript src/prepare_library.R &&
+conda run -n tima Rscript src/prepare_adducts.R &&
 bash src/get_example_isdb.sh &&
 bash src/get_gnverifier.sh &&
-Rscript src/prepare_gnps.R &&
-Rscript src/prepare_isdb.R &&
-Rscript src/prepare_edges.R &&
-Rscript src/prepare_features_components.R &&
-Rscript src/prepare_features_classification.R &&
-Rscript src/prepare_taxa.R &&
-Rscript src/process_annotations.R
+conda run -n tima Rscript src/prepare_gnps.R &&
+conda run -n tima Rscript src/prepare_isdb.R &&
+conda run -n tima Rscript src/prepare_edges.R &&
+conda run -n tima Rscript src/prepare_features_components.R &&
+conda run -n tima Rscript src/prepare_features_classification.R &&
+conda run -n tima Rscript src/prepare_taxa.R &&
+conda run -n tima Rscript src/process_annotations.R
