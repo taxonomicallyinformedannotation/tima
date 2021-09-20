853e28b9787533998aef027469a3ab0cf5841fbd
Adriano Rutz <adriano.rutz@hotmail.com>
2021-09-20 22:19:30 +0000

    Update get_gnverifier.sh

--- src/get_gnverifier.sh
+++ src/get_gnverifier.sh
@@ -9,5 +9,7 @@ eval $(parse_yaml paths.yaml)
 eval $(parse_yaml config/versions.yaml)
 
 mkdir -p $bin_path
-curl -L https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-$OS.tar.gz | tar xOz gnverifier >bin/gnverifier
+wget "https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-$OS.tar.gz" -P bin/
+tar -xzf bin/gnverifier-$gnverifier-$OS.tar.gz -C bin/
+rm -r bin/gnverifier-$gnverifier-$OS.tar.gz
 chmod +x bin/gnverifier
