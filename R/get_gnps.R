41a5f01c5458918e781f99f5a75ed9b14d7065b9
Adafede <adriano.rutz@unige.ch>
2021-11-01 09:06:29 +0100

    Update get_gnps.R

--- R/get_gnps.R
+++ R/get_gnps.R
@@ -3,65 +3,6 @@
 # require(RCurl)
 require(package = readr,quietly = TRUE)
 
-# download_featuretable <- function(id, path = "feature_table.csv") {
-#   #file = paste0("http://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task=",id,"&block=main&file=cluster_buckets/") #Old buckettable format
-#   file = paste(
-#     "http://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task=",
-#     id,
-#     "&block=main&file=quantification_table_reformatted/",
-#     sep = ""
-#   )
-#   download_file(file, path)
-# }
-
-###############################################################################
-
-# download_metadatatable <-
-#   function(id, path = "metadata_table.csv") {
-#     file = paste(
-#       "http://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task=",
-#       id,
-#       "&block=main&file=metadata_table/",
-#       sep = ""
-#     )
-#     download_file(file, path)
-#   }
-
-###############################################################################
-
-#' #' Title
-#' #'
-#' #' @param file
-#' #' @param path
-#' #'
-#' #' @return
-#' #' @export
-#' #'
-#' #' @examples
-#' download_file <- function(file, path = "NULL") {
-#'   if (is.null(path)) {
-#'     error("Must enter a path")
-#'   }
-#'   f <- RCurl::CFILE(path, mode = "wb")
-#'   RCurl::curlPerform(url = file, writedata = f@ref)
-#'   close(f)
-#' }
-
-###############################################################################
-
-# download_cytoscapedata <- function(id, path = "buckettable.tsv") {
-#   #file = paste0("http://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task=",id,"&block=main&file=cluster_buckets/") #Old buckettable format
-#   file = paste(
-#     "http://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task=",
-#     id,
-#     "&block=main&file=quantification_table_reformatted/",
-#     sep = ""
-#   )
-#   download_file(file, path)
-# }
-
-###############################################################################
-
 #' Title
 #'
 #' @param id
