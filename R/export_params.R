621c572296342304547fe326cb021a365e37e2c1
Adafede <adriano.rutz@unige.ch>
2021-11-08 22:27:07 +0100

    update

--- R/export_params.R
+++ R/export_params.R
@@ -14,23 +14,23 @@ source(file = here::here("R", "log_debug.R"))
 #' @examples
 export_params <- function(parameters, directory, step) {
   ifelse(
-    test = !dir.exists(paths$data$interim$path),
-    yes = dir.create(paths$data$interim$path),
-    no = paste(paths$data$interim$path, "exists")
+    test = !dir.exists(here::here(paths$data$interim$path)),
+    yes = dir.create(here::here(paths$data$interim$path)),
+    no = paste(here::here(paths$data$interim$path), "exists")
   )
   ifelse(
-    test = !dir.exists(paths$data$interim$config$path),
-    yes = dir.create(paths$data$interim$config$path),
-    no = paste(paths$data$interim$config$path, "exists")
+    test = !dir.exists(here::here(paths$data$interim$config$path)),
+    yes = dir.create(here::here(paths$data$interim$config$path)),
+    no = paste(here::here(paths$data$interim$config$path), "exists")
   )
   log_debug(
     x = "... path to used parameters is",
-    paths$data$interim$config$path
+    here::here(paths$data$interim$config$path)
   )
 
   yaml::write_yaml(
     x = parameters,
-    file = file.path(
+    file = here::here(file.path(
       directory,
       paste0(
         format(Sys.time(), "%y%m%d_%H%M%OS"),
@@ -38,6 +38,6 @@ export_params <- function(parameters, directory, step) {
         step,
         ".yaml"
       )
-    )
+    ))
   )
 }
