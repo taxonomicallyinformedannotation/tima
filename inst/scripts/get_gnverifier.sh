bea689437a4f9dceeb24081b733422364e26f4d6
Adafede <adriano.rutz@unige.ch>
2021-11-10 15:25:38 +0100

    adapting for Windows

--- inst/scripts/get_gnverifier.sh
+++ inst/scripts/get_gnverifier.sh
@@ -8,8 +8,14 @@ eval $(parse_yaml paths.yaml)
 eval $(parse_yaml config/versions.yaml)
 
 mkdir -p $bin_path
-wget "https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-$OS.tar.gz" -P bin/
-tar -xzf bin/gnverifier-$gnverifier-$OS.tar.gz -C bin/
-rm -r bin/gnverifier-$gnverifier-$OS.tar.gz
-chmod +x bin/gnverifier
+if [[ "$OS"="mac" || "$OS"="linux" ]]; then
+	wget "https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-$OS.tar.gz" -P bin/
+	tar -xzf bin/gnverifier-$gnverifier-$OS.tar.gz -C bin/
+	rm -r bin/gnverifier-$gnverifier-$OS.tar.gz
+else
+	wget "https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-win-64.zip" -P bin/
+	unzip bin/gnverifier-$gnverifier-win-64.zip -d bin/
+	rm -r bin/gnverifier-$gnverifier-win-64.zip
+fi
+
 
