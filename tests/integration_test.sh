46a53573e8bcd8e19d328b6eee0e5b4db4750e16
Adafede <adriano.rutz@unige.ch>
2021-11-11 09:42:00 +0100

    update

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -2,15 +2,15 @@
 
 cp -R config/default config/params &&
 bash inst/scripts/get_lotus.sh &&
-conda run -n tima Rscript inst/scripts/prepare_lotus.R &&
-conda run -n tima Rscript inst/scripts/prepare_library.R &&
-conda run -n tima Rscript inst/scripts/prepare_adducts.R &&
+Rscript inst/scripts/prepare_lotus.R &&
+Rscript inst/scripts/prepare_library.R &&
+Rscript inst/scripts/prepare_adducts.R &&
 bash inst/scripts/get_example_isdb.sh &&
 bash inst/scripts/get_gnverifier.sh &&
-conda run -n tima Rscript inst/scripts/prepare_gnps.R &&
-conda run -n tima Rscript inst/scripts/prepare_isdb.R &&
-conda run -n tima Rscript inst/scripts/prepare_edges.R &&
-conda run -n tima Rscript inst/scripts/prepare_features_components.R &&
-conda run -n tima Rscript inst/scripts/prepare_features_classification.R &&
-conda run -n tima Rscript inst/scripts/prepare_taxa.R &&
-conda run -n tima Rscript inst/scripts/process_annotations.R
+Rscript inst/scripts/prepare_gnps.R &&
+Rscript inst/scripts/prepare_isdb.R &&
+Rscript inst/scripts/prepare_edges.R &&
+Rscript inst/scripts/prepare_features_components.R &&
+Rscript inst/scripts/prepare_features_classification.R &&
+Rscript inst/scripts/prepare_taxa.R &&
+Rscript inst/scripts/process_annotations.R
