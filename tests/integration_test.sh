3a107e1ead5bcbad12d52e520f5feafe65a8d263
Adafede <adriano.rutz@unige.ch>
2021-11-11 10:31:56 +0100

    update

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-cp -R config/default config/params &&
-bash inst/scripts/get_lotus.sh &&
-Rscript inst/scripts/prepare_lotus.R &&
-Rscript inst/scripts/prepare_library.R &&
-Rscript inst/scripts/prepare_adducts.R &&
-bash inst/scripts/get_example_isdb.sh &&
-bash inst/scripts/get_gnverifier.sh &&
-Rscript inst/scripts/prepare_gnps.R &&
-Rscript inst/scripts/prepare_isdb.R &&
-Rscript inst/scripts/prepare_edges.R &&
-Rscript inst/scripts/prepare_features_components.R &&
-Rscript inst/scripts/prepare_features_classification.R &&
-Rscript inst/scripts/prepare_taxa.R &&
+cp -R config/default config/params
+bash inst/scripts/get_lotus.sh
+Rscript inst/scripts/prepare_lotus.R
+Rscript inst/scripts/prepare_library.R
+Rscript inst/scripts/prepare_adducts.R
+bash inst/scripts/get_example_isdb.sh
+bash inst/scripts/get_gnverifier.sh
+Rscript inst/scripts/prepare_gnps.R
+Rscript inst/scripts/prepare_isdb.R
+Rscript inst/scripts/prepare_edges.R
+Rscript inst/scripts/prepare_features_components.R
+Rscript inst/scripts/prepare_features_classification.R
+Rscript inst/scripts/prepare_taxa.R
 Rscript inst/scripts/process_annotations.R
