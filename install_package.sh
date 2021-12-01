f87a12c6bf43ecdc95004c89c7f02450b9da29a5
Adafede <adriano.rutz@unige.ch>
2021-12-01 12:05:48 +0100

    update

--- install_package.sh
+++ install_package.sh
@@ -1,2 +1,2 @@
-Rscript -e 'if(!require(devtools)){install.packages(devtools)}
+Rscript -e 'if(!require(devtools)){install.packages("devtools")}
 devtools::install_github("taxonomicallyinformedannotation/tima-r")'
