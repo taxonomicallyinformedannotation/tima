0c421c9fa201f1bc1363ce34db89d2ac41ec54da
Adafede <adriano.rutz@unige.ch>
2021-12-07 10:00:26 +0100

    update

--- R/prepare_sirius.R
+++ R/prepare_sirius.R
@@ -10,191 +10,191 @@
 prepare_sirius <-
   function(input_directory = params$directory,
            output = params$output) {
-    stopifnot("Your input directory does not exist" = dir.exists(input_directory))
-
-    log_debug("Loading and formatting SIRIUS results")
-    canopus <-
-      readr::read_delim(file = file.path(
-        input_directory,
-        "canopus_summary.tsv"
-      ))
-
-    canopus_adducts <-
-      readr::read_delim(file = file.path(
-        input_directory,
-        "canopus_summary_adducts.tsv"
-      ))
-
-    formula <-
-      readr::read_delim(file = file.path(
-        input_directory,
-        "formula_identifications.tsv"
-      ))
-
-    formula_adducts <-
-      readr::read_delim(file = file.path(
-        input_directory,
-        "formula_identifications_adducts.tsv"
-      ))
-
-    compound <-
-      readr::read_delim(file = file.path(
-        input_directory,
-        "compound_identifications.tsv"
-      ))
-
-    compound_adducts <-
-      readr::read_delim(file = file.path(
-        input_directory,
-        "compound_identifications_adducts.tsv"
-      ))
-
-    ## TODO compound classes if npclassifier one day
-    canopus_prepared <- canopus |>
-      dplyr::mutate(feature_id = gsub(
-        pattern = ".*_",
-        replacement = "",
-        x = name
-      ))
-
-    ## TODO compound classes if npclassifier one day
-    canopus_adducts_prepared <- canopus_adducts |>
-      dplyr::mutate(feature_id = gsub(
-        pattern = ".*_",
-        replacement = "",
-        x = name
-      ))
-
-    ## TODO score not optimal
-    compound_prepared <- compound |>
-      dplyr::mutate(
-        feature_id = gsub(
-          pattern = ".*_",
-          replacement = "",
-          x = id
-        ),
-        score_input = ifelse(
-          test = ConfidenceScore != "N/A",
-          yes = ConfidenceScore,
-          no = -10 / `CSI:FingerIDScore`
-        )
-      ) |>
-      dplyr::select(
-        feature_id,
-        smiles,
-        inchikey_2D = InChIkey2D,
-        molecular_formula = molecularFormula,
-        score_input
-      ) |>
-      dplyr::mutate(
-        library = "SIRIUS",
-        inchikey = NA,
-        smiles_2D = smiles,
-        structure_taxonomy_npclassifier_01pathway = NA,
-        structure_taxonomy_npclassifier_02superclass = NA,
-        structure_taxonomy_npclassifier_03class = NA
-      )
-
-    compound_adducts_prepared <- compound_adducts |>
-      dplyr::mutate(
-        feature_id = gsub(
-          pattern = ".*_",
-          replacement = "",
-          x = id
-        ),
-        score_input = ifelse(
-          test = ConfidenceScore != "N/A",
-          yes = ConfidenceScore,
-          no = -10 / `CSI:FingerIDScore`
-        )
-      ) |>
-      dplyr::select(
-        feature_id,
-        smiles,
-        inchikey_2D = InChIkey2D,
-        molecular_formula = molecularFormula,
-        score_input
-      ) |>
-      dplyr::mutate(
-        library = "SIRIUS",
-        inchikey = NA,
-        smiles_2D = smiles,
-        structure_taxonomy_npclassifier_01pathway = NA,
-        structure_taxonomy_npclassifier_02superclass = NA,
-        structure_taxonomy_npclassifier_03class = NA
-      )
-
-    formula_prepared <- formula |>
-      dplyr::mutate(feature_id = gsub(
-        pattern = ".*_",
-        replacement = "",
-        x = id
-      )) |>
-      mutate(structure_exact_mass = ionMass - `massErrorPrecursor(ppm)` * ionMass * 0.000001) |>
-      distinct(feature_id, molecular_formula = molecularFormula, structure_exact_mass)
-
-    formula_adducts_prepared <- formula_adducts |>
-      dplyr::mutate(feature_id = gsub(
-        pattern = ".*_",
-        replacement = "",
-        x = id
-      )) |>
-      mutate(structure_exact_mass = ionMass - `massErrorPrecursor(ppm)` * ionMass * 0.000001) |>
-      distinct(feature_id, molecular_formula = molecularFormula, structure_exact_mass)
-
-    compounds_prepared <-
-      rbind(compound_prepared, compound_adducts_prepared) |>
-      distinct()
-
-    formulas_prepared <-
-      rbind(formula_prepared, formula_adducts_prepared) |>
-      distinct()
-
-    table <- left_join(compounds_prepared, formulas_prepared)
-
-    table[] <-
-      lapply(
-        table,
-        function(x) {
-          y_as_na(x, y = "N/A")
-        }
-      )
-
-    log_debug(x = "Exporting ...")
-    ifelse(
-      test = !dir.exists(paths$data$path),
-      yes = dir.create(paths$data$path),
-      no = paste(paths$data$path, "exists")
-    )
-    ifelse(
-      test = !dir.exists(paths$data$interim$path),
-      yes = dir.create(paths$data$interim$path),
-      no = paste(paths$data$interim$path, "exists")
-    )
-    ifelse(
-      test = !dir.exists(paths$data$interim$config$path),
-      yes = dir.create(paths$data$interim$config$path),
-      no = paste(paths$data$interim$config$path, "exists")
-    )
-    ifelse(
-      test = !dir.exists(dirname(output)),
-      yes = dir.create(dirname(output)),
-      no = paste(dirname(output), "exists")
-    )
-
-    log_debug(
-      x = "... path to export is",
-      output
-    )
-    readr::write_delim(
-      x = table,
-      file = output,
-      delim = "\t"
-    )
-
-    export_params(
-      parameters = params,
-      directory = paths$data$interim$config$path,
-      step = step
-    )
+    # stopifnot("Your input directory does not exist" = dir.exists(input_directory))
+    #
+    # log_debug("Loading and formatting SIRIUS results")
+    # canopus <-
+    #   readr::read_delim(file = file.path(
+    #     input_directory,
+    #     "canopus_summary.tsv"
+    #   ))
+    #
+    # canopus_adducts <-
+    #   readr::read_delim(file = file.path(
+    #     input_directory,
+    #     "canopus_summary_adducts.tsv"
+    #   ))
+    #
+    # formula <-
+    #   readr::read_delim(file = file.path(
+    #     input_directory,
+    #     "formula_identifications.tsv"
+    #   ))
+    #
+    # formula_adducts <-
+    #   readr::read_delim(file = file.path(
+    #     input_directory,
+    #     "formula_identifications_adducts.tsv"
+    #   ))
+    #
+    # compound <-
+    #   readr::read_delim(file = file.path(
+    #     input_directory,
+    #     "compound_identifications.tsv"
+    #   ))
+    #
+    # compound_adducts <-
+    #   readr::read_delim(file = file.path(
+    #     input_directory,
+    #     "compound_identifications_adducts.tsv"
+    #   ))
+    #
+    # ## TODO compound classes if npclassifier one day
+    # canopus_prepared <- canopus |>
+    #   dplyr::mutate(feature_id = gsub(
+    #     pattern = ".*_",
+    #     replacement = "",
+    #     x = name
+    #   ))
+    #
+    # ## TODO compound classes if npclassifier one day
+    # canopus_adducts_prepared <- canopus_adducts |>
+    #   dplyr::mutate(feature_id = gsub(
+    #     pattern = ".*_",
+    #     replacement = "",
+    #     x = name
+    #   ))
+    #
+    # ## TODO score not optimal
+    # compound_prepared <- compound |>
+    #   dplyr::mutate(
+    #     feature_id = gsub(
+    #       pattern = ".*_",
+    #       replacement = "",
+    #       x = id
+    #     ),
+    #     score_input = ifelse(
+    #       test = ConfidenceScore != "N/A",
+    #       yes = ConfidenceScore,
+    #       no = -10 / `CSI:FingerIDScore`
+    #     )
+    #   ) |>
+    #   dplyr::select(
+    #     feature_id,
+    #     smiles,
+    #     inchikey_2D = InChIkey2D,
+    #     molecular_formula = molecularFormula,
+    #     score_input
+    #   ) |>
+    #   dplyr::mutate(
+    #     library = "SIRIUS",
+    #     inchikey = NA,
+    #     smiles_2D = smiles,
+    #     structure_taxonomy_npclassifier_01pathway = NA,
+    #     structure_taxonomy_npclassifier_02superclass = NA,
+    #     structure_taxonomy_npclassifier_03class = NA
+    #   )
+    #
+    # compound_adducts_prepared <- compound_adducts |>
+    #   dplyr::mutate(
+    #     feature_id = gsub(
+    #       pattern = ".*_",
+    #       replacement = "",
+    #       x = id
+    #     ),
+    #     score_input = ifelse(
+    #       test = ConfidenceScore != "N/A",
+    #       yes = ConfidenceScore,
+    #       no = -10 / `CSI:FingerIDScore`
+    #     )
+    #   ) |>
+    #   dplyr::select(
+    #     feature_id,
+    #     smiles,
+    #     inchikey_2D = InChIkey2D,
+    #     molecular_formula = molecularFormula,
+    #     score_input
+    #   ) |>
+    #   dplyr::mutate(
+    #     library = "SIRIUS",
+    #     inchikey = NA,
+    #     smiles_2D = smiles,
+    #     structure_taxonomy_npclassifier_01pathway = NA,
+    #     structure_taxonomy_npclassifier_02superclass = NA,
+    #     structure_taxonomy_npclassifier_03class = NA
+    #   )
+    #
+    # formula_prepared <- formula |>
+    #   dplyr::mutate(feature_id = gsub(
+    #     pattern = ".*_",
+    #     replacement = "",
+    #     x = id
+    #   )) |>
+    #   mutate(structure_exact_mass = ionMass - `massErrorPrecursor(ppm)` * ionMass * 0.000001) |>
+    #   distinct(feature_id, molecular_formula = molecularFormula, structure_exact_mass)
+    #
+    # formula_adducts_prepared <- formula_adducts |>
+    #   dplyr::mutate(feature_id = gsub(
+    #     pattern = ".*_",
+    #     replacement = "",
+    #     x = id
+    #   )) |>
+    #   mutate(structure_exact_mass = ionMass - `massErrorPrecursor(ppm)` * ionMass * 0.000001) |>
+    #   distinct(feature_id, molecular_formula = molecularFormula, structure_exact_mass)
+    #
+    # compounds_prepared <-
+    #   rbind(compound_prepared, compound_adducts_prepared) |>
+    #   distinct()
+    #
+    # formulas_prepared <-
+    #   rbind(formula_prepared, formula_adducts_prepared) |>
+    #   distinct()
+    #
+    # table <- left_join(compounds_prepared, formulas_prepared)
+    #
+    # table[] <-
+    #   lapply(
+    #     table,
+    #     function(x) {
+    #       y_as_na(x, y = "N/A")
+    #     }
+    #   )
+    #
+    # log_debug(x = "Exporting ...")
+    # ifelse(
+    #   test = !dir.exists(paths$data$path),
+    #   yes = dir.create(paths$data$path),
+    #   no = paste(paths$data$path, "exists")
+    # )
+    # ifelse(
+    #   test = !dir.exists(paths$data$interim$path),
+    #   yes = dir.create(paths$data$interim$path),
+    #   no = paste(paths$data$interim$path, "exists")
+    # )
+    # ifelse(
+    #   test = !dir.exists(paths$data$interim$config$path),
+    #   yes = dir.create(paths$data$interim$config$path),
+    #   no = paste(paths$data$interim$config$path, "exists")
+    # )
+    # ifelse(
+    #   test = !dir.exists(dirname(output)),
+    #   yes = dir.create(dirname(output)),
+    #   no = paste(dirname(output), "exists")
+    # )
+    #
+    # log_debug(
+    #   x = "... path to export is",
+    #   output
+    # )
+    # readr::write_delim(
+    #   x = table,
+    #   file = output,
+    #   delim = "\t"
+    # )
+    #
+    # export_params(
+    #   parameters = params,
+    #   directory = paths$data$interim$config$path,
+    #   step = step
+    # )
   }
