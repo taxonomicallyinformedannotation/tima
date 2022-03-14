548beb3ba35c3560ce461ebaf50b3dc3fe1a0b24
Adafede <adriano.rutz@unige.ch>
2022-03-14 10:55:02 +0100

    style

--- R/get_example_feature_table.R
+++ R/get_example_feature_table.R
@@ -11,23 +11,29 @@ get_example_feature_table <-
   function(url = paths$urls$example_feature_table,
            export = paths$data$interim$annotations$example_feature_table) {
     paths <- parse_yaml_paths()
-    
+
     ifelse(
       test = !dir.exists(dirname(dirname(export))),
       yes = dir.create(dirname(dirname(export))),
-      no = paste(dirname(dirname(export)),
-                 "exists")
+      no = paste(
+        dirname(dirname(export)),
+        "exists"
+      )
     )
     ifelse(
       test = !dir.exists(dirname(export)),
       yes = dir.create(dirname(export)),
-      no = paste(dirname(export),
-                 "exists")
+      no = paste(
+        dirname(export),
+        "exists"
+      )
     )
-    
+
     readr::read_tsv(file = url) |>
-      dplyr::select(feature_id = `cluster index`,
-                    rt = RTMean,
-                    mz = `precursor mass`) |>
+      dplyr::select(
+        feature_id = `cluster index`,
+        rt = RTMean,
+        mz = `precursor mass`
+      ) |>
       readr::write_tsv(file = export)
   }
