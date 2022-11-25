700000a1981fb445f52296263285b14b7f01c025
Adriano Rutz <adriano.rutz@hotmail.com>
2022-11-25 10:45:03 +0100

    formatting

--- inst/scripts/get_example_isdb.R
+++ inst/scripts/get_example_isdb.R
@@ -28,9 +28,9 @@ get_example_isdb <-
   function(url = paths$urls$example_isdb,
            export = paths$data$interim$annotations$example_isdb) {
     paths <- parse_yaml_paths()
-    
+
     create_dir(export = export)
-    
+
     readr::read_tsv(file = url) |>
       readr::write_tsv(file = export)
   }
