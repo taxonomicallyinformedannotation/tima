68698eca0ab6b6010b3ded0af64d58f2973cb6d5
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2022-12-26 13:51:45 +0100

    code documentation improvement

--- R/replace_gnps_job_id.R
+++ R/replace_gnps_job_id.R
@@ -1,20 +1,20 @@
 #' @title Replace GNPS job ID
 #'
-#' @param x a character string containing a GNPS job ID
+#' @param job a character string containing a GNPS job ID
 #' @param default a default values for a GNPS job ID
-#' @param params a user-specified values for a GNPS job ID
+#' @param repl a user-specified values for a GNPS job ID
 #'
 #' @return a character string with the GNPS job ID modified according to the rules specified in the function
 #'
 #' @export
 #'
-#' @examples replaced_job_id <- replace_gnps_job_id("MyGNPSJobID", yamls_default, params)
-replace_gnps_job_id <- function(x, default = yamls_default$prepare_params$gnps, params = params$gnps) {
+#' @examples replaced_job_id <- replace_gnps_job_id("MyGNPSJobID", yamls_default, user)
+replace_gnps_job_id <- function(job, default = yamls_default$prepare_params$gnps, repl = params$gnps) {
   # Replace the default GNPS job ID with the user-specified GNPS job ID
   replaced_job_id <- gsub(
     pattern = default,
-    replacement = params,
-    x = x
+    replacement = repl,
+    x = job
   )
 
   return(replaced_job_id)
