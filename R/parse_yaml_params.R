d0964c0ee359e739e7de87edd129bc2192ade222
Adriano Rutz <adriano.rutz@hotmail.com>
2023-02-14 14:25:37 +0100

    targets

--- R/parse_yaml_params.R
+++ R/parse_yaml_params.R
@@ -2,6 +2,9 @@
 #'
 #' @description This function parses YAML parameters
 #'
+#' @param def Default path
+#' @param par Param path
+#'
 #' @return A list containing the parameters specified in the YAML files
 #'
 #' @export
@@ -10,23 +13,23 @@
 #' @importFrom yaml read_yaml
 #'
 #' @examples NULL
-parse_yaml_params <- function() {
+parse_yaml_params <- function(def = default_path ,par = params_path) {
   # Initialize an empty list to store the parameters
   params <- list()
 
   # Read the default YAML file
   log_debug("Loading yaml parameters")
   suppressWarnings(params <-
-    yaml::read_yaml(file = default_path, handlers = list(
+    yaml::read_yaml(file = def, handlers = list(
       seq = function(x) {
         purrr::list_flatten(x)
       }
     )))
 
   # If a user-specified YAML file exists, read it and overwrite the default values with the user-specified ones
-  if (file.exists(params_path)) {
+  if (file.exists(par)) {
     suppressWarnings(params <-
-      yaml::read_yaml(file = params_path, handlers = list(
+      yaml::read_yaml(file = par, handlers = list(
         seq = function(x) {
           purrr::list_flatten(x)
         }
