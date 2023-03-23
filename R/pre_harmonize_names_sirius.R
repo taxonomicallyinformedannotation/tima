2ef16a7e810406aca18d6afe765fdfbf877adebf
Adriano Rutz <adriano.rutz@hotmail.com>
2023-03-23 08:57:01 +0100

    fix

--- R/pre_harmonize_names_sirius.R
+++ R/pre_harmonize_names_sirius.R
@@ -12,7 +12,7 @@
 pre_harmonize_names_sirius <- function(x) {
   # Remove any characters after and including the '/' character from the name
   y <- x |>
-    stringr::str_remove(pattern = stringr::fixed(pattern = "/.*"))
+    stringr::str_remove(pattern = "/.*")
 
   return(y)
 }
