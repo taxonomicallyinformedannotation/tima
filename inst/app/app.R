50de1fb36aaf08051a9066be1fc221122766c408
Adriano Rutz <adriano.rutz@hotmail.com>
2023-03-27 09:14:29 +0200

    update

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,9 +1,7 @@
 library(shiny)
 library(timaR)
 
-if (grepl(pattern = "tima-r$", x = getwd())) {
-  setwd(dir = "inst/app")
-}
+setwd(dir = "inst/app")
 
 source(file = "ui.R")
 source(file = "server.R")
