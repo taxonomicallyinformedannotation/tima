4d2f44749440e3f1a95e126ed3c116e08ed8a30c
Adriano Rutz <adriano.rutz@hotmail.com>
2023-03-28 16:38:33 +0200

    install

--- inst/scripts/install.R
+++ inst/scripts/install.R
@@ -1,10 +1,11 @@
 ## Simple install helper
 options(repos = c(CRAN = "https://cloud.r-project.org"))
-if (!requireNamespace("devtools", quietly = TRUE)) {
-  install.packages("devtools")
+if (!requireNamespace("pak", quietly = TRUE)) {
+  install.packages("pak")
 }
 # if (!requireNamespace("renv", quietly = TRUE)) {
 #   install.packages("renv")
 # }
 # renv::restore()
-devtools::install()
+pak::local_install(upgrade = TRUE,
+                   dependencies = TRUE)
