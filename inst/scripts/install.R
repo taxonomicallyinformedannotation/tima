15a775154573b084d468dd83e2e6e7f1bea72484
Adriano Rutz <adriano.rutz@hotmail.com>
2023-03-29 10:37:20 +0200

    update

--- inst/scripts/install.R
+++ inst/scripts/install.R
@@ -1,31 +1,28 @@
 ## Simple install helper
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-# if (!requireNamespace("pak", quietly = TRUE)) {
-#   stream <- "devel"
-#   install.packages(
-#     "pak",
-#     repos = sprintf(
-#       "https://r-lib.github.io/p/pak/%s/%s/%s/%s",
-#       stream,
-#       .Platform$pkgType,
-#       R.Version()$os,
-#       R.Version()$arch
-#     )
-#   )
-# }
-# needs <-
-#   sprintf("Config/Needs/%s", strsplit("website", "[[:space:],]+")[[1]])
-# deps <- strsplit("deps::., any::sessioninfo", "[[:space:],]+")[[1]]
-# extra_deps <- strsplit("any::pkgdown", "[[:space:],]+")[[1]]
-# pak::lockfile_create(c(deps, extra_deps),
-#                      dependencies = c(needs, ("all")))
-# pak::lockfile_install(update = FALSE)
-# pak::local_install(upgrade = FALSE)
-if (!requireNamespace("remotes", quietly = TRUE)) {
-  install.packages("remotes")
+if (!requireNamespace("pak", quietly = TRUE)) {
+  stream <- "devel"
+  install.packages(
+    "pak",
+    repos = sprintf(
+      "https://r-lib.github.io/p/pak/%s/%s/%s/%s",
+      stream,
+      .Platform$pkgType,
+      R.Version()$os,
+      R.Version()$arch
+    )
+  )
 }
-if (!requireNamespace("desc", quietly = TRUE)) {
-  install.packages("desc")
+if (Sys.info()[["sysname"]] == "Windows") {
+  if (!requireNamespace("installr", quietly = TRUE)) {
+    install.packages("installr")
+  }
+  installr::install.rtools()
 }
-desc::desc_get_urls()[[1]] |>
-remotes::install_github()
+needs <-
+  sprintf("Config/Needs/%s", strsplit("website", "[[:space:],]+")[[1]])
+deps <- strsplit("deps::., any::sessioninfo", "[[:space:],]+")[[1]]
+extra_deps <- strsplit("any::pkgdown", "[[:space:],]+")[[1]]
+pak::lockfile_create(c(deps, extra_deps),
+                     dependencies = c(needs, ("all")))
+pak::lockfile_install()
+pak::local_install()
