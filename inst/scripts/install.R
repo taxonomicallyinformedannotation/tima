10c823cc116f639ef9f81a4bcbb392ad9e740e59
Adriano Rutz <adriano.rutz@hotmail.com>
2023-03-29 11:14:55 +0200

    update

--- inst/scripts/install.R
+++ inst/scripts/install.R
@@ -2,6 +2,12 @@
 if (!requireNamespace("renv", quietly = TRUE)) {
   install.packages("renv")
 }
+if (Sys.info()[["sysname"]] == "Windows") {
+  if (!requireNamespace("installr", quietly = TRUE)) {
+    install.packages("installr")
+  }
+  installr::install.rtools()
+}
 if (Sys.info()[["sysname"]] != "Linux") {
   renv::equip()
 }
