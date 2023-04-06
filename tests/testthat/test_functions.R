a33e01d89065dc0f98ba74e61ff0cbfc1d9aa89a
Adriano Rutz <adriano.rutz@hotmail.com>
2023-04-06 17:18:03 +0200

    update

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -282,9 +282,6 @@ testthat::test_that("Whole process", {
     output = params$files$libraries$spectral$exp |>
       gsub(pattern = ".sqlite", replacement = ".mgf")
   )
-  ## missing due to current .rds export
-  spectra <- params$files$libraries$spectral$exp |>
-    import_spectra()
 
   ### HMDB
   # prepare_isdb_hmdb()
