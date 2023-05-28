f164effee8d434be837a643b7e483b2ef861dbf0
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2023-05-28 06:34:26 +0200

    Update get_organism_taxonomy_ott.R

--- R/get_organism_taxonomy_ott.R
+++ R/get_organism_taxonomy_ott.R
@@ -167,10 +167,12 @@ get_organism_taxonomy_ott <- function(df,
         ott_ids = otts,
         include_lineage = TRUE,
         include_terminal_descendants = TRUE
-      )
+      )|>
+        data.frame()
 
       taxon_lineage <- taxon_info |>
-        rotl::tax_lineage()
+        rotl::tax_lineage()|>
+        data.frame()
 
       list_df <- list()
 
