0b60c02de2c545129cacfc71c125f68b56866c9f
Adriano Rutz <adriano.rutz@hotmail.com>
2023-08-11 09:17:31 +0200

    feat(create_edges_spectra): add MS1 only spectra processing

--- R/create_edges_spectra.R
+++ R/create_edges_spectra.R
@@ -49,69 +49,82 @@ create_edges_spectra <- function(
 
   log_debug("Loading spectra...")
   spectra <- input |>
-    import_spectra() |>
-    ## TODO make it a param
-    sanitize_spectra(cutoff = qutoff, deeper = TRUE) |>
-    Spectra::addProcessing(remove_above_precursor(),
-      spectraVariables = c("precursorMz")
-    ) |>
-    Spectra::addProcessing(normalize_peaks()) |>
-    Spectra::applyProcessing()
+    import_spectra()
+  if (length(spectra) > 0) {
+    spectra <- spectra |>
+      ## TODO make it a param
+      sanitize_spectra(cutoff = qutoff, deeper = TRUE) |>
+      Spectra::addProcessing(remove_above_precursor(),
+        spectraVariables = c("precursorMz")
+      ) |>
+      Spectra::addProcessing(normalize_peaks()) |>
+      Spectra::applyProcessing()
 
-  log_debug("Performing spectral comparison")
-  log_debug(
-    "As we do not limit the precursors delta,
+    log_debug("Performing spectral comparison")
+    log_debug(
+      "As we do not limit the precursors delta,
       expect a (relatively) long processing time."
-  )
-  log_debug("Take yourself a break, you deserve it.")
-  nspecz <- length(spectra)
-  precz <- spectra$precursorMz
-  fragz <- spectra@backend@peaksData
-
-  edges <-
-    pbapply::pblapply(
-      X = 1:(nspecz - 1),
-      FUN = create_edges,
-      frags = fragz,
-      precs = precz,
-      nspecs = nspecz,
-      ms2_tolerance = dalton,
-      ppm_tolerance = ppm,
-      threshold = threshold
-    ) |>
-    dplyr::bind_rows()
-
-  edges <- edges |>
-    tidytable::select(
-      !!as.name(name_source) := "feature_id",
-      !!as.name(name_target) := "target_id",
-      tidytable::everything()
     )
+    log_debug("Take yourself a break, you deserve it.")
+    nspecz <- length(spectra)
+    precz <- spectra$precursorMz
+    fragz <- spectra@backend@peaksData
 
-  ## TODO find a way to have consistency in spectrum IDs
-  idz <- spectra@backend@spectraData$acquisitionNum
-  edges <- edges |>
-    dplyr::mutate(
-      name_source = idz[name_source],
-      name_target = idz[name_target]
-    )
+    edges <-
+      pbapply::pblapply(
+        X = 1:(nspecz - 1),
+        FUN = create_edges,
+        frags = fragz,
+        precs = precz,
+        nspecs = nspecz,
+        ms2_tolerance = dalton,
+        ppm_tolerance = ppm,
+        threshold = threshold
+      ) |>
+      dplyr::bind_rows()
 
-  edges <- edges |>
-    tidytable::select(tidytable::any_of(
-      c(
-        name_source,
-        name_target,
-        "score",
-        "reverse_score",
-        "presence_ratio",
-        "count_peaks_matched"
+    edges <- edges |>
+      tidytable::select(
+        !!as.name(name_source) := "feature_id",
+        !!as.name(name_target) := "target_id",
+        tidytable::everything()
       )
-    ))
 
-  edges <- edges |>
-    dplyr::filter(
-      score >= threshold
+    ## TODO find a way to have consistency in spectrum IDs
+    idz <- spectra@backend@spectraData$acquisitionNum
+    edges <- edges |>
+      dplyr::mutate(
+        name_source = idz[name_source],
+        name_target = idz[name_target]
+      )
+
+    edges <- edges |>
+      tidytable::select(tidytable::any_of(
+        c(
+          name_source,
+          name_target,
+          "score",
+          "reverse_score",
+          "presence_ratio",
+          "count_peaks_matched"
+        )
+      ))
+
+    edges <- edges |>
+      dplyr::filter(
+        score >= threshold
+      )
+  } else {
+    log_debug("No spectra were found, returning an empty dataframe instead")
+    edges <- tidytable::tidytable(
+      !!as.name(name_source) := NA,
+      !!as.name(name_target) := NA,
+      "score" = NA,
+      "reverse_score" = NA,
+      "presence_ratio" = NA,
+      "count_peaks_matched" = NA
     )
+  }
 
   export_params(step = "create_edges_spectra")
   export_output(x = edges, file = output[[1]])
