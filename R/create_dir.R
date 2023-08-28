b66a152cc85c34789127754cde223a45c3751867
Adriano Rutz <adriano.rutz@hotmail.com>
2023-08-28 14:52:22 +0200

    cleanup

--- R/create_dir.R
+++ R/create_dir.R
@@ -14,21 +14,13 @@ create_dir <- function(export) {
   ## Check if the export path includes a file name
   if (grepl(pattern = ".", x = export, fixed = TRUE)) {
     dirname_path <- dirname(export)
-    file_name <- basename(export)
   } else {
     dirname_path <- export
-    file_name <- ""
   }
 
   ## Create the directory at the specified path if it does not exist
   if (!dir.exists(dirname_path)) {
     dir.create(dirname_path, recursive = TRUE)
     log_debug("Directory", dirname_path, "created.")
-  } else {
-    log_debug("Directory", dirname_path, "already exists.")
-  }
-
-  if (file_name != "") {
-    log_debug("File", file_name, "will be saved in the directory", dirname_path)
   }
 }
