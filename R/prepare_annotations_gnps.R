83e34ef8165239e756b28053336f3ddaf8ec1a1d
Adriano Rutz <adriano.rutz@hotmail.com>
2023-09-06 11:01:53 +0200

    small fix

--- R/prepare_annotations_gnps.R
+++ R/prepare_annotations_gnps.R
@@ -53,7 +53,7 @@ prepare_annotations_gnps <-
         tidytable::select(tidytable::any_of(c(
           "feature_id" = "#Scan#",
           ## TODO check which one is correct
-          # "candidate_structure_error_mz" = "MassDiff",
+          "candidate_structure_error_mz" = "MassDiff",
           "candidate_library" = "LibraryName",
           ## TODO "candidate_library_type" = "TODO",
           "candidate_structure_name" = "Compound_Name",
