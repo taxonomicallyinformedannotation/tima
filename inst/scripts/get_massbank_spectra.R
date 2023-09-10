69134882f823d34bbdd541f128e67e062681b7c2
Adriano Rutz <adriano.rutz@hotmail.com>
2023-09-10 09:08:40 +0200

    feat(inst/scripts): add MassBank spectra download script

---
+++ inst/scripts/get_massbank_spectra.R
@@ -0,0 +1,19 @@
+start <- Sys.time()
+
+require(
+  package = "timaR",
+  quietly = TRUE
+)
+
+log_debug(
+  "This script",
+  crayon::green("downloads MassBank spectra \n")
+)
+log_debug("Authors: ", crayon::green("AR"), "\n")
+log_debug("Contributors: ...")
+
+targets::tar_make(names = matches("lib_spe_exp_mb_raw"))
+
+end <- Sys.time()
+
+log_debug("Script finished in", crayon::green(format(end - start)))
