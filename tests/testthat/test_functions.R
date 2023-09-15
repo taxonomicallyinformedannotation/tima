faa26033484de41325dd5b9406d17f1799669d93
Adriano Rutz <adriano.rutz@hotmail.com>
2023-09-15 10:07:56 +0200

    fix(linters and test files): improve markdown filter regex to exclude json files and update test function setup

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -6,6 +6,7 @@ testthat::test_that("Test functions", {
     command =
       "
     mkdir inst
+    cp -R app inst/app
     cp -R params inst/params
     cp -R scripts inst/scripts ## because of docopt steps
     cp paths.yaml inst/paths.yaml
