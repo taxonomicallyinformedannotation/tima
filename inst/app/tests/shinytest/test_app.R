09665c6f109bf5999c671eabae1b2c4ab56c752c
Adriano Rutz <adriano.rutz@hotmail.com>
2023-09-15 12:38:34 +0200

    fix(shiny tests): update expected output and file paths

--- inst/app/tests/shinytest/test_app.R
+++ inst/app/tests/shinytest/test_app.R
@@ -1,35 +1,22 @@
 ## Test the app
 testthat::test_that("Test app", {
   ## Needed for testing
-  setwd("../../../../")
-  system(
-    command =
-      "
-    mkdir inst
-    cp -R params inst/params
-    cp paths.yaml inst/paths.yaml
-    "
-  )
-  paths <- parse_yaml_paths()
-  params <- get_params(step = "prepare_params")
-  get_gnps_tables(
-    filename = "example",
-    path_features = paths$data$source$features,
-    path_metadata = paths$data$source$metadata,
-    path_spectra = paths$data$source$spectra,
-    gnps_job_id = params$gnps$id
+  setwd("../../..")
+  get_file(
+    url = "https://github.com/taxonomicallyinformedannotation/tima-example-files/raw/main/example_spectra_mini.mgf",
+    export = "data/source/example_spectra.mgf"
   )
   get_file(
-    url = paths$urls$examples$spectra_mini,
-    export = paths$data$source$spectra
+    url = "https://github.com/taxonomicallyinformedannotation/tima-example-files/raw/main/example_features.csv",
+    export = "data/source/example_features.csv"
   )
-  setwd("inst/app/")
+  setwd("app/")
 
   ## Actual testing
   app <- ShinyDriver$new()
   app$snapshotInit("test_app")
-  app$uploadFile(fil_spe_raw = "../../data/source/example_spectra.mgf")
-  app$uploadFile(fil_fea_raw = "../../data/source/example_features.csv")
+  app$uploadFile(fil_spe_raw = "../data/source/example_spectra.mgf")
+  app$uploadFile(fil_fea_raw = "../data/source/example_features.csv")
   app$setInputs(save = "click")
   app$snapshot()
   testthat::succeed()
