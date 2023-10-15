887c83be9965899c2a14d2f7979d80edcdf743cc
Adriano Rutz <adriano.rutz@hotmail.com>
2023-10-15 13:27:41 +0200

    fix(auth): update Zenodo file retrieval logic to fix weird behavior with new URL format

--- R/get_file.R
+++ R/get_file.R
@@ -35,11 +35,24 @@ get_file <-
     # )
 
     if (!file.exists(export)) {
-      options(timeout = limit)
-      create_dir(export = export)
-      utils::download.file(
-        url = url,
-        destfile = export
+      tryCatch(
+        expr = {
+          httr2::request(base_url = url) |>
+            httr2::req_error(
+              is_error = function(resp) {
+                return(FALSE)
+              }
+            ) |>
+            httr2::req_perform()
+          options(timeout = limit)
+          create_dir(export = export)
+          utils::download.file(url = url,
+                               destfile = export)
+        },
+        error = function(e) {
+          cat("There was an unexpected problem downloading your file.")
+          return(export)
+        }
       )
     } else {
       message("File already exists. Skipping.")
