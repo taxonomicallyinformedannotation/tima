d32810b52fa131ae9dd58577bc3b4351670bc3a1
Adriano Rutz <adriano.rutz@hotmail.com>
2023-10-18 18:27:01 +0200

    shiny port

--- inst/app/R/server.R
+++ inst/app/R/server.R
@@ -91,7 +91,6 @@ server <- function(input, output, session) {
         setwd("../..")
         targets::tar_watch_server(id = "tar_watch")
         targets::tar_watch(
-          host = "0.0.0.0",
           port = 3839,
           display = "graph",
           displays = c("summary", "graph"),
--- inst/scripts/run_app.R
+++ inst/scripts/run_app.R
@@ -1,5 +1,5 @@
+options(shiny.host="0.0.0.0")
 shiny::runApp(
   appDir = "inst/app",
-  host = "0.0.0.0",
   port = 3838
 )
