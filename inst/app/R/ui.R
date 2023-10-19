7c2987224c3f16dd420ccfbbc87ad456e0e9b64d
Adriano Rutz <adriano.rutz@hotmail.com>
2023-10-19 08:29:50 +0200

    shiny app

--- inst/app/R/ui.R
+++ inst/app/R/ui.R
@@ -1,5 +1,23 @@
 options(shiny.host = "0.0.0.0")
 
+#' @title Label mandatory
+#'
+#' @description This function adds an asterisk to mandatory inputs
+#'
+#' @param label Label
+#'
+#' @return NULL
+#'
+#' @export
+#'
+#' @examples NULL
+label_mandatory <- function(label) {
+  shiny::tagList(
+    label,
+    shiny::span("*", class = "mandatory_star")
+  )
+}
+
 app_css <-
   ".mandatory_star { color: red; }
      .shiny-input-container { margin-top: 25px; }
