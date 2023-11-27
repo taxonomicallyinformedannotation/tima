5642e5f2db2ef05dfa054fb31e86a91696737d5b
Adriano Rutz <adriano.rutz@hotmail.com>
2023-11-27 13:43:18 +0100

    fix(pak): fix pak installation issue

--- inst/scripts/install.R
+++ inst/scripts/install.R
@@ -7,28 +7,26 @@ if (Sys.info()[["sysname"]] == "Windows") {
     GUI = FALSE
   )
 }
-lib <- Sys.getenv("R_LIBS_SITE")
-if (length(lib) == 0) {
-  lib <- file.path(dirname(.Library), "site-library")
-  cat(sprintf("R_LIBS_SITE=%s\n", lib), append = TRUE)
-  cat(sprintf("R_LIB_FOR_PAK=%s\n", lib), append = TRUE)
-
-  message("Setting R_LIBS_SITE to ", lib)
-} else {
-  message("R_LIBS_SITE is already set to ", lib)
-  cat(
-    sprintf(
-      "R_LIB_FOR_PAK=%s\n",
-      strsplit(lib, .Platform$path.sep)[[1]][[1]]
-    ),
-    append = TRUE
-  )
-}
-dir.create(lib, showWarnings = FALSE, recursive = TRUE)
 if (!requireNamespace("pak", quietly = TRUE)) {
+  lib <- Sys.getenv("R_LIBS_SITE")
+  if (lib == "") {
+    lib <- file.path(dirname(.Library), "site-library")
+    cat(sprintf("R_LIBS_SITE=%s\n", lib), append = TRUE)
+    cat(sprintf("R_LIB_FOR_PAK=%s\n", lib), append = TRUE)
+
+    message("Setting R_LIBS_SITE to ", lib)
+  } else {
+    message("R_LIBS_SITE is already set to ", lib)
+    cat(
+      sprintf(
+        "R_LIB_FOR_PAK=%s\n",
+        strsplit(lib, .Platform$path.sep)[[1]][[1]]
+      ),
+      append = TRUE
+    )
+  }
   install.packages(
     "pak",
-    lib = lib,
     repos = sprintf(
       "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
       .Platform$pkgType,
@@ -38,17 +36,15 @@ if (!requireNamespace("pak", quietly = TRUE)) {
   )
 }
 pak::pak_update()
-pak::lockfile_create(lib = lib)
-pak::lockfile_install(lib = lib)
+pak::lockfile_create()
+pak::lockfile_install()
 unlink(x = "pkg.lock")
 pak::pak(
   ask = FALSE,
-  upgrade = FALSE,
-  lib = lib
+  upgrade = FALSE
 )
 pak::pkg_install(
   pkg = desc::desc_get_urls()[[1]],
   ask = FALSE,
-  upgrade = FALSE,
-  lib = lib
+  upgrade = FALSE
 )
