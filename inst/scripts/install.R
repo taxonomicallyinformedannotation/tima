63b3c95c798a02ee45788c37b742fa04a19c9271
Adriano Rutz <adriano.rutz@hotmail.com>
2023-11-27 12:53:52 +0100

    fix(inst scripts): fix pak issue with r_libs_site setting

--- inst/scripts/install.R
+++ inst/scripts/install.R
@@ -22,8 +22,8 @@ if (!requireNamespace("pak", quietly = TRUE)) {
     )
   )
 }
-lib <- Sys.getenv("R_LIBS_SITE")
-if (lib == "") {
+lib <- .Library.site
+if (length(lib) == 0) {
   lib <- file.path(dirname(.Library), "site-library")
   cat(sprintf("R_LIBS_SITE=%s\n", lib), append = TRUE)
   cat(sprintf("R_LIB_FOR_PAK=%s\n", lib), append = TRUE)
