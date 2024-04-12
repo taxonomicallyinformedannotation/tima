f4e5b85ac0963ccf24b23cb0a12d578efdb28c5b
Adriano Rutz <adriano.rutz@hotmail.com>
2024-04-12 09:49:31 +0200

    Migrate to shinytest2

---
+++ inst/app/tests/testthat/test-shinytest2.R
@@ -0,0 +1,19 @@
+library(shinytest2)
+library(timaR)
+
+test_that("{shinytest2} recording: app", {
+  get_file(
+    url = "https://github.com/taxonomicallyinformedannotation/tima-example-files/raw/main/example_spectra_mini.mgf",
+    export = "../data/source/example_spectra.mgf"
+  )
+  get_file(
+    url = "https://github.com/taxonomicallyinformedannotation/tima-example-files/raw/main/example_features.csv",
+    export = "../data/source/example_features.csv"
+  )
+
+  app <- AppDriver$new()
+  app$upload_file(fil_spe_raw = "../data/source/example_spectra.mgf")
+  app$upload_file(fil_fea_raw = "../data/source/example_features.csv")
+  app$click("save")
+  app$expect_values()
+})
