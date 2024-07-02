527bb59c8a3e91fe018a88afd432dc47516f8fb3
Adriano Rutz <adriano.rutz@hotmail.com>
2024-07-02 18:39:25 +0200

    Improve install_latest_version.R

--- R/install_latest_version.R
+++ R/install_latest_version.R
@@ -20,10 +20,10 @@ install_latest_version <- function(test = FALSE) {
   if (!requireNamespace("pak", quietly = TRUE)) {
     install.packages("pak")
   }
-  if (!requireNamespace("devtools", quietly = TRUE)) {
-    install.packages("devtools")
+  if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes")
   }
-  devtools::install_github(repo = "taxonomicallyinformedannotation/tima-r")
+  remotes::install_github(repo = "taxonomicallyinformedannotation/tima-r")
   cache <- fs::path_home(".tima")
   message("Creating cache at ", cache)
   fs::dir_create(path = cache)
