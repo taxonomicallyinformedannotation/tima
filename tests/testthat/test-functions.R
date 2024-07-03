b2506f55a646fb7faa65d3ede66ce08dc2a0878e
Adriano Rutz <adriano.rutz@hotmail.com>
2024-07-03 17:34:27 +0200

    fix(testthat): test functions compatibility with pak and timaR packages

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -2,10 +2,10 @@ library(testthat)
 ## need to do all in one because of outputs needed in the same temp dir
 ## use fixtures instead in the future
 test_that(desc = "Test functions", code = {
-  if (requireNamespace("pak")) {
+  if ("pak" %in% installed.packages()) {
     remove.packages("pak")
   }
-  if (requireNamespace("timaR")) {
+  if ("timaR" %in% installed.packages()) {
     remove.packages("timaR")
   }
   install_latest_version()
