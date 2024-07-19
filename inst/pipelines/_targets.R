da0782e6d37d340fd45292825ed4bb21ca20abd1
Adriano Rutz <adriano.rutz@hotmail.com>
2024-07-19 23:09:46 +0200

    fix(inst/WORDLIST and pipelines/_targets.R): add ORCID to wordlist and load packages in R scripts

--- inst/pipelines/_targets.R
+++ inst/pipelines/_targets.R
@@ -5,7 +5,7 @@
 
 # Load packages required to define the pipeline:
 library(targets)
-
+pkgload::load_all()
 # Set target options:
 tar_option_set(
   packages = "timaR",
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,5 +1,7 @@
 library(testthat)
 library(timaR)
+pkgload::load_all()
+
 ## need to do all in one because of outputs needed in the same temp dir
 ## use fixtures instead in the future
 test_that(desc = "Test functions", code = {
