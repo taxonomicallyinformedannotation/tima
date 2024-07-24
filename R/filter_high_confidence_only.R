990cdee0e0437aa615bd3cb06d48e914c6c8cb02
Adriano Rutz <adriano.rutz@hotmail.com>
2024-07-24 11:35:33 +0200

    docs: improve logging and formatting in R filter script

--- R/filter_high_confidence_only.R
+++ R/filter_high_confidence_only.R
@@ -1,3 +1,5 @@
+import::from(crayon, blue, .into = environment())
+import::from(crayon, green, .into = environment())
 import::from(crayon, red, .into = environment())
 import::from(tidytable, filter, .into = environment())
 
@@ -6,6 +8,8 @@ import::from(tidytable, filter, .into = environment())
 #'
 #' @description This function filters highly confident annotations only.
 #'
+#' @importFrom crayon blue
+#' @importFrom crayon green
 #' @importFrom crayon red
 #' @importFrom tidytable filter
 #'
@@ -27,10 +31,19 @@ filter_high_confidence_only <-
     # TODO this is very basic for now but already massively filters.
     # TODO Later implement SIRIUS/internal library filters.
     df <- df |>
-      filter(score_biological >= score_bio_min |
-        candidate_score_pseudo_initial >= score_ini_min)
+      filter(
+        score_biological >= score_bio_min |
+          candidate_score_pseudo_initial >= score_ini_min
+      )
     after <- nrow(df)
-    log_debug("Removed", red(before - after), "low confidence candidates")
+    log_debug(
+      "Removed",
+      red(before - after),
+      "low confidence candidates out of the",
+      blue(before),
+      "total ones."
+    )
+    log_debug(green(after), "high confidence candidates remaining.")
     rm(before, after)
     return(df)
   }
