4ef3db35e1a045be2f92344d639f4a725cfd6bef
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-01 23:19:26 +0200

    fix tests

--- R/extract_spectra.R
+++ R/extract_spectra.R
@@ -1,12 +1,9 @@
-import::from(Spectra, peaksData, .into = environment())
-import::from(Spectra, spectraData, .into = environment())
 import::from(tidytable, across, .into = environment())
 import::from(tidytable, any_of, .into = environment())
 import::from(tidytable, as_tidytable, .into = environment())
 import::from(tidytable, everything, .into = environment())
 import::from(tidytable, group_by, .into = environment())
 import::from(tidytable, mutate, .into = environment())
-import::from(tidytable, reframe, .into = environment())
 import::from(tidytable, rename, .into = environment())
 import::from(tidytable, select, .into = environment())
 
@@ -14,15 +11,12 @@ import::from(tidytable, select, .into = environment())
 #'
 #' @description This function extracts spectra from a `Spectra`object
 #'
-#' @importFrom Spectra peaksData
-#' @importFrom Spectra spectraData
 #' @importFrom tidytable across
 #' @importFrom tidytable any_of
 #' @importFrom tidytable as_tidytable
 #' @importFrom tidytable everything
 #' @importFrom tidytable group_by
 #' @importFrom tidytable mutate
-#' @importFrom tidytable reframe
 #' @importFrom tidytable rename
 #' @importFrom tidytable select
 #'
@@ -44,23 +38,18 @@ extract_spectra <- function(object) {
   incoherent_integer <- c("spectrum_id")
   incoherent_numeric <- c("PrecursorMZ")
 
-  ## Extract peaks data and transform it into a data frame
-  peaks <- object |>
-    peaksData() |>
-    data.frame() |>
-    as_tidytable() |>
-    group_by(group) |>
-    reframe(across(.cols = everything(), .fns = list))
-
   ## Extract spectra data and transform it into a data frame
-  spectra <- object |>
-    spectraData() |>
+  spectra <- object@backend@spectraData |>
     data.frame() |>
     as_tidytable()
 
   ## Add 'mz' and 'intensity' columns from peaks data
-  spectra$mz <- peaks$mz
-  spectra$intensity <- peaks$intensity
+  spectra$mz <- lapply(object@backend@peaksData, function(peakData) {
+    peakData[, 1]
+  })
+  spectra$intensity <- lapply(object@backend@peaksData, function(peakData) {
+    peakData[, 2]
+  })
 
   ## Synonyms issue
   # spectra <- spectra |>
