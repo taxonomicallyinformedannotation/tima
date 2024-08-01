ad614dee0e5f87b4783fc7a76202a6b7260d165c
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-01 10:40:33 +0200

    fix tests

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -4,7 +4,6 @@ library(tima)
 ## need to do all in one because of outputs needed in the same temp dir
 ## use fixtures instead in the future
 test_that(desc = "Test functions", code = {
-  try(remove.packages("pak"))
   install()
   # When already present
   install()
