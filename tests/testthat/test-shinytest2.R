592166529e8bd46186f4fd14a5c5f1fcfbc931a2
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-02 00:39:16 +0200

    test: shinytest2 timeout increased to reduce loading times

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -5,8 +5,8 @@ test_that("{shinytest2} recording: app", {
   get_file(url = "https://github.com/taxonomicallyinformedannotation/tima-example-files/raw/main/example_spectra_mini.mgf", export = "data/source/example_spectra.mgf")
   get_file(url = "https://github.com/taxonomicallyinformedannotation/tima-example-files/raw/main/example_features.csv", export = "data/source/example_features.csv")
   app <- AppDriver$new(
-    timeout = 20000,
-    load_timeout = 20000,
+    timeout = 100000,
+    load_timeout = 100000,
     shiny_args = list(
       appDir = system.file(package = "tima"),
       host = "127.0.0.1",
