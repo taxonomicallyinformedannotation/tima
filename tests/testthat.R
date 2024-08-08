c6ca599ca4a5be23efcc006708552776d198dc93
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2024-08-08 19:49:20 +0200

    feat(testthat): update test package test to reflect changes

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
 library(shinytest2)
 library(testthat)
 
-## Test the package
-test_package(package = "tima")
-
 ## Test the app
 test_app(app_dir = "..", filter = "shinytest2")
+
+## Test the package
+test_package(package = "tima")
