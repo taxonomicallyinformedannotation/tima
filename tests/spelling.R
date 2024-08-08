79ccb2c48d6224349bf7f13cd832f0fa125eb34d
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-08 11:15:12 +0200

    fix(test-coverage workflow and spelling tests)

--- tests/spelling.R
+++
@@ -1,6 +0,0 @@
-library(spelling)
-spell_check_test(
-  vignettes = TRUE,
-  error = FALSE,
-  skip_on_cran = TRUE
-)
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,14 @@
 library(shinytest2)
+library(spelling)
 library(testthat)
 library(tima)
+
+## Test spelling
+spell_check_test(
+  vignettes = TRUE,
+  error = FALSE,
+  skip_on_cran = TRUE
+)
 
 ## Test the package
 test_package(package = "tima")
