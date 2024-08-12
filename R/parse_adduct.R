b60fd21ce5b668f19e541bdc0b3a1f237a024017
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-12 08:35:03 +0200

    doc

--- R/parse_adduct.R
+++ R/parse_adduct.R
@@ -19,7 +19,8 @@ import::from(stringi, stri_split_regex, .into = environment())
 #'
 #' @export
 #'
-#' @examples NULL
+#' @examples parse_adduct("[M+H]+")
+#' parse_adduct("[2M1-C6H12O6 (hexose)+NaCl+H]2+")
 parse_adduct <- function(adduct_string, regex = "\\[(\\d*)M(?![a-z])(\\d*)([+-][\\w\\d].*)?.*\\](\\d*)([+-])?") {
   ## Full match
   matches <- stri_match_all_regex(str = adduct_string, pattern = regex)[[1]]
