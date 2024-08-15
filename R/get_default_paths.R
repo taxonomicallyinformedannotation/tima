f1dfdba4f588eb3a7772b4cb85101bd0b23f1b48
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-15 11:03:27 +0200

    rm setwd

--- R/get_default_paths.R
+++ R/get_default_paths.R
@@ -11,12 +11,5 @@
 #'
 #' @examples get_default_paths()
 get_default_paths <- function(yaml = system.file("extdata", "paths.yaml", package = "tima")) {
-  ## Read the YAML file containing the paths
-  paths <- yaml::read_yaml(file = yaml)
-
-  ## Set the working directory to the base directory specified in the YAML file
-  setwd(paths$base_dir)
-
-  ## Return the list of paths
-  return(paths)
+  return(yaml::read_yaml(file = yaml))
 }
