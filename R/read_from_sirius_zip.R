92cf799b8b795908e3b17a3a1a3dcdca0b6f14ed
Adriano Rutz <adriano.rutz@hotmail.com>
2025-02-04 17:07:52 +0100

    Support `.rar` compressed SIRIUS workspaces (#186)

--- R/read_from_sirius_zip.R
+++ R/read_from_sirius_zip.R
@@ -14,13 +14,29 @@ read_from_sirius_zip <- function(sirius_zip, file) {
       pattern = ".zip",
       replacement = "",
       fixed = TRUE
+    ) |>
+    gsub(
+      pattern = ".rar",
+      replacement = "",
+      fixed = TRUE
     )
   if (sirius_zip != sirius_no_zip) {
     temp <- tempdir()
     sirius_dir <- file.path(temp, sirius_no_zip |>
       basename())
     if (!file.exists(sirius_dir)) {
-      utils::unzip(sirius_zip, exdir = temp)
+      if (sirius_zip |> endsWith(".zip")) {
+        utils::unzip(sirius_zip, exdir = temp)
+      }
+      if (sirius_zip |> endsWith(".rar")) {
+        utils::untar(sirius_zip, exdir = temp)
+        sirius_dir <- sirius_dir |>
+          gsub(
+            pattern = sirius_no_zip |>
+              basename(),
+            replacement = ""
+          )
+      }
     }
   } else {
     sirius_dir <- sirius_no_zip
