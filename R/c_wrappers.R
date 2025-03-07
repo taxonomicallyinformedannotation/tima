10bb23f649187f03fa0da0dcadc411d76116149a
Adriano Rutz <adriano.rutz@hotmail.com>
2025-03-07 13:02:08 +0100

    wrap C code in R functions for `targets` (see https://github.com/ropensci/targets/issues/721)

--- R/c_wrappers.R
+++ R/c_wrappers.R
@@ -1,23 +1,27 @@
+## This was done because of targets re-running steps calling C code
+## See https://github.com/ropensci/targets/issues/721
+## Probably having them return a hash of the source C file would be even better
+
 #' Wrapper for the C function "gnps"
 #'
-#' @param x_mat A numeric matrix.
-#' @param y_mat A numeric matrix.
+#' @param x A numeric matrix.
+#' @param y A numeric matrix.
 #' @return The result from the C function.
-gnps_wrapper <- function(x_mat, y_mat) {
-  .Call("gnps", x = x_mat, y = y_mat)
+gnps_wrapper <- function(x, y) {
+  .Call("gnps", x = x, y = y)
 }
 
 #' Wrapper for the C function "join_gnps"
 #'
-#' @param query_masses Numeric vector or matrix for query masses.
-#' @param target_masses Numeric vector or matrix for target masses.
-#' @param query_precursor Numeric vector of precursor values for queries.
-#' @param target_precursor Numeric vector of precursor values for targets.
-#' @param dalton Numeric value specifying the tolerance in daltons.
+#' @param x Numeric vector or matrix for query masses.
+#' @param y Numeric vector or matrix for target masses.
+#' @param xPrecursorMz Numeric vector of precursor values for queries.
+#' @param yPrecursorMz Numeric vector of precursor values for targets.
+#' @param tolerance Numeric value specifying the tolerance in daltons.
 #' @param ppm Numeric value specifying the tolerance in ppm.
 #' @return The result returned from the C function "join_gnps".
 #' @export
-join_gnps_wrapper <- function(query_masses, target_masses, query_precursor, target_precursor, dalton, ppm) {
+join_gnps_wrapper <- function(x, y, xPrecursorMz, yPrecursorMz, tolerance, ppm) {
   .Call(
     "join_gnps",
     x = query_masses,
