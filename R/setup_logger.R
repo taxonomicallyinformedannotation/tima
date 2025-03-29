82fad40f471ac0b6fce2dcfaaaea425ff836e6fd
Adriano Rutz <adriano.rutz@hotmail.com>
2025-03-29 10:57:50 +0100

    feat(auth): improve logger configuration and update codemeta file size

--- R/setup_logger.R
+++ R/setup_logger.R
@@ -4,6 +4,7 @@
 #' @return NULL
 #' @keywords internal
 setup_logger <- function(filename) {
+  # logger::log_layout(logger::layout_glue_colors)
   logger::log_threshold(logger::TRACE)
   logger::log_appender(
     appender = logger::appender_tee(
