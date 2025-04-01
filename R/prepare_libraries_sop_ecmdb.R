99c9751fc0022b03dc97eb91b3d9b29a3f2ebddc
Adriano Rutz <adriano.rutz@hotmail.com>
2025-04-01 17:22:27 +0200

    opti

--- R/prepare_libraries_sop_ecmdb.R
+++ R/prepare_libraries_sop_ecmdb.R
@@ -28,87 +28,89 @@ prepare_libraries_sop_ecmdb <-
       step = "prepare_libraries_sop_ecmdb"
     )$files$libraries$sop$prepared$ecmdb
   ) {
-    if (file.exists(input)) {
-      logger::log_trace("Loading ECMDB resources")
+    if (!file.exists(output) && file.size(output) > 100000) {
+      if (file.exists(input)) {
+        logger::log_trace("Loading ECMDB resources")
 
-      file <- gsub(
-        pattern = ".zip",
-        replacement = "",
-        x = input,
-        fixed = TRUE
-      ) |>
-        gsub(
-          pattern = ".*/",
+        file <- gsub(
+          pattern = ".zip",
           replacement = "",
-          perl = TRUE
+          x = input,
+          fixed = TRUE
+        ) |>
+          gsub(
+            pattern = ".*/",
+            replacement = "",
+            perl = TRUE
+          )
+
+        ecmdb <- jsonlite::stream_in(
+          con = unz(description = input, filename = file),
+          verbose = FALSE
+        ) |>
+          data.frame() |>
+          tidytable::as_tidytable()
+
+        logger::log_trace("Formatting ECMDB")
+        ecmdb_prepared <- ecmdb |>
+          tidytable::mutate(
+            structure_inchikey_2D = stringi::stri_sub(
+              str = moldb_inchikey,
+              from = 1,
+              to = 14
+            ),
+            structure_smiles_2D = NA_character_
+          ) |>
+          tidytable::rename(
+            structure_name = name,
+            structure_inchikey = moldb_inchikey,
+            structure_smiles = moldb_smiles,
+            structure_molecular_formula = moldb_formula,
+            structure_exact_mass = moldb_mono_mass,
+            structure_xlogp = moldb_logp
+          ) |>
+          tidytable::mutate(
+            structure_taxonomy_npclassifier_01pathway = NA_character_,
+            structure_taxonomy_npclassifier_02superclass = NA_character_,
+            structure_taxonomy_npclassifier_03class = NA_character_,
+            structure_taxonomy_classyfire_chemontid = NA_character_,
+            structure_taxonomy_classyfire_01kingdom = NA_character_,
+            structure_taxonomy_classyfire_02superclass = NA_character_,
+            structure_taxonomy_classyfire_03class = NA_character_,
+            structure_taxonomy_classyfire_04directparent = NA_character_
+          ) |>
+          tidytable::mutate(
+            organism_name = "Escherichia coli",
+            organism_taxonomy_ottid = 474506,
+            organism_taxonomy_01domain = "Bacteria",
+            organism_taxonomy_02kingdom = NA_character_,
+            organism_taxonomy_03phylum = "Proteobacteria",
+            organism_taxonomy_04class = "Gammaproteobacteria",
+            organism_taxonomy_05order = "Enterobacteriales",
+            organism_taxonomy_06family = "Enterobacteriaceae",
+            organism_taxonomy_07tribe = NA_character_,
+            organism_taxonomy_08genus = NA_character_,
+            organism_taxonomy_09species = "Escherichia coli",
+            organism_taxonomy_10varietas = NA_character_
+          ) |>
+          tidytable::mutate(reference_doi = NA) |>
+          select_sop_columns() |>
+          round_reals() |>
+          tidytable::distinct()
+        rm(ecmdb)
+      } else {
+        logger::log_warn(
+          "Sorry, ECMDB not found, returning an empty file instead"
         )
+        ecmdb_prepared <- fake_sop_columns()
+      }
 
-      ecmdb <- jsonlite::stream_in(
-        con = unz(description = input, filename = file),
-        verbose = FALSE
-      ) |>
-        data.frame() |>
-        tidytable::as_tidytable()
-
-      logger::log_trace("Formatting ECMDB")
-      ecmdb_prepared <- ecmdb |>
-        tidytable::mutate(
-          structure_inchikey_2D = stringi::stri_sub(
-            str = moldb_inchikey,
-            from = 1,
-            to = 14
-          ),
-          structure_smiles_2D = NA_character_
-        ) |>
-        tidytable::rename(
-          structure_name = name,
-          structure_inchikey = moldb_inchikey,
-          structure_smiles = moldb_smiles,
-          structure_molecular_formula = moldb_formula,
-          structure_exact_mass = moldb_mono_mass,
-          structure_xlogp = moldb_logp
-        ) |>
-        tidytable::mutate(
-          structure_taxonomy_npclassifier_01pathway = NA_character_,
-          structure_taxonomy_npclassifier_02superclass = NA_character_,
-          structure_taxonomy_npclassifier_03class = NA_character_,
-          structure_taxonomy_classyfire_chemontid = NA_character_,
-          structure_taxonomy_classyfire_01kingdom = NA_character_,
-          structure_taxonomy_classyfire_02superclass = NA_character_,
-          structure_taxonomy_classyfire_03class = NA_character_,
-          structure_taxonomy_classyfire_04directparent = NA_character_
-        ) |>
-        tidytable::mutate(
-          organism_name = "Escherichia coli",
-          organism_taxonomy_ottid = 474506,
-          organism_taxonomy_01domain = "Bacteria",
-          organism_taxonomy_02kingdom = NA_character_,
-          organism_taxonomy_03phylum = "Proteobacteria",
-          organism_taxonomy_04class = "Gammaproteobacteria",
-          organism_taxonomy_05order = "Enterobacteriales",
-          organism_taxonomy_06family = "Enterobacteriaceae",
-          organism_taxonomy_07tribe = NA_character_,
-          organism_taxonomy_08genus = NA_character_,
-          organism_taxonomy_09species = "Escherichia coli",
-          organism_taxonomy_10varietas = NA_character_
-        ) |>
-        tidytable::mutate(reference_doi = NA) |>
-        select_sop_columns() |>
-        round_reals() |>
-        tidytable::distinct()
-      rm(ecmdb)
-    } else {
-      logger::log_warn(
-        "Sorry, ECMDB not found, returning an empty file instead"
+      export_params(
+        parameters = get_params(step = "prepare_libraries_sop_ecmdb"),
+        step = "prepare_libraries_sop_ecmdb"
       )
-      ecmdb_prepared <- fake_sop_columns()
+      export_output(x = ecmdb_prepared, file = output)
+      rm(ecmdb_prepared)
     }
-
-    export_params(
-      parameters = get_params(step = "prepare_libraries_sop_ecmdb"),
-      step = "prepare_libraries_sop_ecmdb"
-    )
-    export_output(x = ecmdb_prepared, file = output)
-    rm(ecmdb_prepared)
     return(output)
   }
