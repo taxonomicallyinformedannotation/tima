9e83baf88cefc1b21b4d46de798f5b1acb973875
Adriano Rutz <adriano.rutz@hotmail.com>
2025-05-13 21:33:27 +0200

    up

--- R/filter_high_confidence_only.R
+++ R/filter_high_confidence_only.R
@@ -7,6 +7,7 @@
 #' @param score_bio_min Minimal biological score. Current default to 0.85.
 #' @param score_ini_min Minimal initial score. Current default to 0.95.
 #' @param score_final_min Minimal final score. Current default to 0.75.
+#' @param error_rt_max Maximal RT error. Current default to 0.1.
 #'
 #' @return A dataframe containing only high confidence annotations
 #'
@@ -16,7 +17,8 @@ filter_high_confidence_only <-
     df,
     score_bio_min = 0.85,
     score_ini_min = 0.95,
-    score_final_min = 0.75
+    score_final_min = 0.75,
+    error_rt_max = 0.1
   ) {
     logger::log_trace("Keeping high confidence candidates only")
     before <- nrow(df)
@@ -28,6 +30,13 @@ filter_high_confidence_only <-
           candidate_score_pseudo_initial >= score_ini_min |
           score_weighted_chemo >= score_final_min
       )
+    if ("candidate_structure_error_rt" %in% colnames(df)) {
+      df <- df |>
+        tidytable::filter(
+          (is.na(candidate_structure_error_rt) |
+            candidate_structure_error_rt < error_rt_max)
+        )
+    }
     after <- nrow(df)
     logger::log_info(
       "Removed {before - after} low confidence candidates out of the {before} total ones."
