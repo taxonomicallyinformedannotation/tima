0fd6f8cd23974b76a28bc6565f87eea1ada22406
Adriano Rutz <adriano.rutz@hotmail.com>
2025-11-05 00:31:46 +0100

    feat(auth): improve spectral similarity network edge creation and GNPS annotation preparation

--- R/prepare_annotations_gnps.R
+++ R/prepare_annotations_gnps.R
@@ -1,19 +1,21 @@
 #' @title Prepare annotations GNPS
 #'
-#' @description This function prepares GNPS obtained annotations
+#' @description This function prepares GNPS spectral library matching results
+#'     by standardizing column names, integrating structure metadata, and
+#'     formatting for downstream TIMA annotation workflows.
 #'
 #' @include get_params.R
 #' @include select_annotations_columns.R
 #'
-#' @param input Input file
-#' @param output Output file
-#' @param str_stereo File containing structures stereo
-#' @param str_met File containing structures metadata
-#' @param str_nam File containing structures names
-#' @param str_tax_cla File containing Classyfire taxonomy
-#' @param str_tax_npc File containing NPClassifier taxonomy
+#' @param input Character string or vector of paths to GNPS annotation files
+#' @param output Character string path for prepared GNPS annotations output
+#' @param str_stereo Character string path to structures stereochemistry file
+#' @param str_met Character string path to structures metadata file
+#' @param str_nam Character string path to structures names file
+#' @param str_tax_cla Character string path to ClassyFire taxonomy file
+#' @param str_tax_npc Character string path to NPClassifier taxonomy file
 #'
-#' @return The path to the prepared GNPS annotations
+#' @return Character string path to prepared GNPS annotations
 #'
 #' @export
 #'
@@ -24,107 +26,132 @@
 #' prepare_annotations_gnps()
 #' unlink("data", recursive = TRUE)
 #' }
-prepare_annotations_gnps <-
-  function(
-    input = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$annotations$raw$spectral$gnps,
-    output = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$annotations$prepared$structural$gnps,
-    str_stereo = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$libraries$sop$merged$structures$stereo,
-    str_met = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$libraries$sop$merged$structures$metadata,
-    str_nam = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$libraries$sop$merged$structures$names,
-    str_tax_cla = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$libraries$sop$merged$structures$taxonomies$cla,
-    str_tax_npc = get_params(
-      step = "prepare_annotations_gnps"
-    )$files$libraries$sop$merged$structures$taxonomies$npc
-  ) {
-    if (length(input) == 0) {
-      input <- "w1llN3v3r3v3r3x1st"
-    }
-    if (
-      all(
-        purrr::map(.x = input, .f = file.exists) |>
-          unlist()
-      )
-    ) {
-      logger::log_trace("Loading and formatting GNPS results")
-      ## See https://github.com/CCMS-UCSD/GNPS_Workflows/issues/747
-      table <- purrr::map(
-        .x = input,
-        .f = tidytable::fread,
-        na.strings = c("", "NA"),
-        colClasses = "character"
-      ) |>
-        tidytable::bind_rows() |>
-        tidytable::mutate(
-          candidate_structure_error_mz = as.numeric(MZErrorPPM) *
-            1E-6 *
-            as.numeric(Precursor_MZ)
-        ) |>
-        tidytable::select(tidyselect::any_of(
-          c(
-            "feature_id" = "#Scan#",
-            "candidate_adduct" = "Adduct",
-            "candidate_structure_error_mz" = "MassDiff",
-            "candidate_library" = "LibraryName",
-            "candidate_structure_name" = "Compound_Name",
-            "candidate_score_similarity" = "MQScore",
-            "candidate_count_similarity_peaks_matched" = "SharedPeaks",
-            "candidate_structure_inchi" = "INCHI",
-            "candidate_structure_inchikey" = "InChIKey",
-            "candidate_structure_inchikey_connectivity_layer" = "InChIKey-Planar",
-            "candidate_structure_tax_npc_01pat" = "npclassifier_pathway",
-            "candidate_structure_tax_npc_02sup" = "npclassifier_superclass",
-            "candidate_structure_tax_npc_03cla" = "npclassifier_class",
-            "candidate_structure_exact_mass" = "ExactMass",
-            ## Only partially present
-            "candidate_structure_tax_cla_02sup" = "superclass",
-            "candidate_structure_tax_cla_03cla" = "class",
-            "candidate_structure_tax_cla_04dirpar" = "subclass"
-          )
-        )) |>
-        tidytable::mutate(
-          candidate_structure_smiles_no_stereo = NA,
-          candidate_structure_molecular_formula = candidate_structure_inchi |>
-            ## really dirty
-            gsub(
-              pattern = ".*\\/C",
-              replacement = "C",
-              perl = TRUE
-            ) |>
-            gsub(
-              pattern = "\\/.*",
-              replacement = "",
-              perl = TRUE
-            ),
-          candidate_structure_xlogp = NA,
-          ## Only partially present
-          candidate_structure_tax_cla_chemontid = NA,
-          candidate_structure_tax_cla_01kin = NA
-        ) |>
-        select_annotations_columns()
-    } else {
-      logger::log_warn(
-        "No GNPS annotations found, returning an empty file instead"
-      )
-      table <- fake_annotations_columns()
-    }
-
-    export_params(
-      parameters = get_params(step = "prepare_annotations_gnps"),
-      step = "prepare_annotations_gnps"
-    )
-    export_output(x = table, file = output[[1]])
-    rm(table)
-    return(output[[1]])
+prepare_annotations_gnps <- function(
+  input = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$annotations$raw$spectral$gnps,
+  output = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$annotations$prepared$structural$gnps,
+  str_stereo = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$libraries$sop$merged$structures$stereo,
+  str_met = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$libraries$sop$merged$structures$metadata,
+  str_nam = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$libraries$sop$merged$structures$names,
+  str_tax_cla = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$libraries$sop$merged$structures$taxonomies$cla,
+  str_tax_npc = get_params(
+    step = "prepare_annotations_gnps"
+  )$files$libraries$sop$merged$structures$taxonomies$npc
+) {
+  # Validate output path
+  if (!is.character(output) || length(output) != 1L) {
+    stop("output must be a single character string")
   }
+
+  # Validate structure file paths
+  str_files <- list(
+    str_stereo = str_stereo,
+    str_met = str_met,
+    str_nam = str_nam,
+    str_tax_cla = str_tax_cla,
+    str_tax_npc = str_tax_npc
+  )
+
+  for (param_name in names(str_files)) {
+    param_value <- str_files[[param_name]]
+    if (!is.character(param_value) || length(param_value) != 1L) {
+      stop(param_name, " must be a single character string")
+    }
+    if (!file.exists(param_value)) {
+      stop(param_name, " file not found: ", param_value)
+    }
+  }
+
+  logger::log_info("Preparing GNPS annotations")
+
+  if (length(input) == 0) {
+    input <- "w1llN3v3r3v3r3x1st"
+  }
+  if (
+    all(
+      purrr::map(.x = input, .f = file.exists) |>
+        unlist()
+    )
+  ) {
+    logger::log_trace("Loading and formatting GNPS results")
+    ## See https://github.com/CCMS-UCSD/GNPS_Workflows/issues/747
+    table <- purrr::map(
+      .x = input,
+      .f = tidytable::fread,
+      na.strings = c("", "NA"),
+      colClasses = "character"
+    ) |>
+      tidytable::bind_rows() |>
+      tidytable::mutate(
+        candidate_structure_error_mz = as.numeric(MZErrorPPM) *
+          1E-6 *
+          as.numeric(Precursor_MZ)
+      ) |>
+      tidytable::select(tidyselect::any_of(
+        c(
+          "feature_id" = "#Scan#",
+          "candidate_adduct" = "Adduct",
+          "candidate_structure_error_mz" = "MassDiff",
+          "candidate_library" = "LibraryName",
+          "candidate_structure_name" = "Compound_Name",
+          "candidate_score_similarity" = "MQScore",
+          "candidate_count_similarity_peaks_matched" = "SharedPeaks",
+          "candidate_structure_inchi" = "INCHI",
+          "candidate_structure_inchikey" = "InChIKey",
+          "candidate_structure_inchikey_connectivity_layer" = "InChIKey-Planar",
+          "candidate_structure_tax_npc_01pat" = "npclassifier_pathway",
+          "candidate_structure_tax_npc_02sup" = "npclassifier_superclass",
+          "candidate_structure_tax_npc_03cla" = "npclassifier_class",
+          "candidate_structure_exact_mass" = "ExactMass",
+          ## Only partially present
+          "candidate_structure_tax_cla_02sup" = "superclass",
+          "candidate_structure_tax_cla_03cla" = "class",
+          "candidate_structure_tax_cla_04dirpar" = "subclass"
+        )
+      )) |>
+      tidytable::mutate(
+        candidate_structure_smiles_no_stereo = NA,
+        candidate_structure_molecular_formula = candidate_structure_inchi |>
+          ## really dirty
+          gsub(
+            pattern = ".*\\/C",
+            replacement = "C",
+            perl = TRUE
+          ) |>
+          gsub(
+            pattern = "\\/.*",
+            replacement = "",
+            perl = TRUE
+          ),
+        candidate_structure_xlogp = NA,
+        ## Only partially present
+        candidate_structure_tax_cla_chemontid = NA,
+        candidate_structure_tax_cla_01kin = NA
+      ) |>
+      select_annotations_columns()
+  } else {
+    logger::log_warn(
+      "No GNPS annotations found, returning an empty file instead"
+    )
+    table <- fake_annotations_columns()
+  }
+
+  export_params(
+    parameters = get_params(step = "prepare_annotations_gnps"),
+    step = "prepare_annotations_gnps"
+  )
+  export_output(x = table, file = output[[1]])
+  rm(table)
+  return(output[[1]])
+}
--- R/prepare_libraries_sop_merged.R
+++ R/prepare_libraries_sop_merged.R
@@ -1,31 +1,34 @@
 #' @title Prepare merged structure organism pairs libraries
 #'
-#' @description This function prepares the libraries made of
-#'    all sub-libraries containing structure-organism pairs
+#' @description This function merges all structure-organism pair libraries
+#'     (LOTUS, HMDB, ECMDB, etc.) into a single comprehensive library. Can
+#'     optionally filter by taxonomic level to create biologically-focused
+#'     subsets. Also splits structures into separate metadata tables.
 #'
-#' @details It can be restricted to specific taxa to have
-#'    more biologically meaningful annotation.
+#' @details Creates merged library by combining all available SOP sources,
+#'     optionally filtering by taxonomic criteria (e.g., only Gentianaceae).
+#'     Splits output into structures metadata, names, taxonomy, and organisms.
 #'
 #' @include get_organism_taxonomy_ott.R
 #' @include get_params.R
 #' @include split_tables_sop.R
 #'
-#' @param files List of libraries to be merged
-#' @param filter Boolean. TRUE or FALSE if you want to filter the library
-#' @param level Biological rank to be filtered.
-#'    Kingdom, phylum, family, genus, ...
-#' @param value Name of the taxon or taxa to be kept,
-#'    e.g. 'Gentianaceae|Apocynaceae'
-#' @param cache Cache where already processed SMILES are located
-#' @param output_key Output file for keys
-#' @param output_org_tax_ott Output file for organisms taxonomy (OTT)
-#' @param output_str_stereo Output file for structures stereo
-#' @param output_str_met Output file for structures metadata
-#' @param output_str_nam Output file for structures names
-#' @param output_str_tax_cla Output file for structures taxonomy (Classyfire)
-#' @param output_str_tax_npc Output file for structures taxonomy (NPC)
+#' @param files Character vector or list of paths to prepared library files
+#' @param filter Logical whether to filter the merged library by taxonomy
+#' @param level Character string taxonomic rank for filtering (kingdom, phylum,
+#'     family, genus, etc.)
+#' @param value Character string taxon name(s) to keep (can use | for multiple,
+#'     e.g., 'Gentianaceae|Apocynaceae')
+#' @param cache Character string path to cache directory for processed SMILES
+#' @param output_key Character string path for output keys file
+#' @param output_org_tax_ott Character string path for organisms taxonomy (OTT) file
+#' @param output_str_stereo Character string path for structures stereochemistry file
+#' @param output_str_met Character string path for structures metadata file
+#' @param output_str_nam Character string path for structures names file
+#' @param output_str_tax_cla Character string path for ClassyFire taxonomy file
+#' @param output_str_tax_npc Character string path for NPClassifier taxonomy file
 #'
-#' @return The path to the prepared structure-organism pairs library MERGED
+#' @return Character string path to the prepared merged SOP library
 #'
 #' @export
 #'
@@ -37,213 +40,231 @@
 #' repo <- "taxonomicallyinformedannotation/tima-example-files/main/"
 #' dir <- paste0(github, repo)
 #' files <- get_params(step = "prepare_libraries_sop_merged")$files$libraries$sop$prepared$lotus |>
-#'   gsub(
-#'     pattern = ".gz",
-#'     replacement = "",
-#'     fixed = TRUE
-#'   )
+#'   gsub(pattern = ".gz", replacement = "", fixed = TRUE)
 #' get_file(url = paste0(dir, files), export = files)
 #' prepare_libraries_sop_merged(files = files)
 #' unlink("data", recursive = TRUE)
 #' }
-prepare_libraries_sop_merged <-
-  function(
-    files = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$prepared,
-    filter = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$organisms$filter$mode,
-    level = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$organisms$filter$level,
-    value = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$organisms$filter$value,
-    cache = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$structures$processed,
-    output_key = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$keys,
-    ## document it above in case
-    # output_org_nam = get_params(step = "prepare_libraries_sop_merged")$files$libraries$sop$merged$organisms$names,
-    output_org_tax_ott = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$organisms$taxonomies$ott,
-    output_str_stereo = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$structures$stereo,
-    output_str_met = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$structures$metadata,
-    output_str_nam = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$structures$names,
-    output_str_tax_cla = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$structures$taxonomies$cla,
-    output_str_tax_npc = get_params(
-      step = "prepare_libraries_sop_merged"
-    )$files$libraries$sop$merged$structures$taxonomies$npc
-  ) {
-    stopifnot(
-      "Your filter parameter must be 'true' or 'false'" = filter %in%
-        c(TRUE, FALSE)
+prepare_libraries_sop_merged <- function(
+  files = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$prepared,
+  filter = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$organisms$filter$mode,
+  level = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$organisms$filter$level,
+  value = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$organisms$filter$value,
+  cache = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$structures$processed,
+  output_key = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$keys,
+  ## document it above in case
+  # output_org_nam = get_params(step = "prepare_libraries_sop_merged")$files$libraries$sop$merged$organisms$names,
+  output_org_tax_ott = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$organisms$taxonomies$ott,
+  output_str_stereo = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$structures$stereo,
+  output_str_met = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$structures$metadata,
+  output_str_nam = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$structures$names,
+  output_str_tax_cla = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$structures$taxonomies$cla,
+  output_str_tax_npc = get_params(
+    step = "prepare_libraries_sop_merged"
+  )$files$libraries$sop$merged$structures$taxonomies$npc
+) {
+  # Validate filter parameter
+  if (!is.logical(filter) || length(filter) != 1L) {
+    stop("filter must be a single logical value (TRUE/FALSE)")
+  }
+
+  # Validate output paths
+  output_paths <- list(
+    output_key = output_key,
+    output_org_tax_ott = output_org_tax_ott,
+    output_str_stereo = output_str_stereo,
+    output_str_met = output_str_met,
+    output_str_nam = output_str_nam,
+    output_str_tax_cla = output_str_tax_cla,
+    output_str_tax_npc = output_str_tax_npc
+  )
+
+  for (param_name in names(output_paths)) {
+    param_value <- output_paths[[param_name]]
+    if (!is.character(param_value) || length(param_value) != 1L) {
+      stop(param_name, " must be a single character string")
+    }
+  }
+
+  logger::log_info("Preparing merged structure-organism pairs library")
+  logger::log_debug("Filter mode: ", filter)
+
+  if (isTRUE(filter)) {
+    # Validate taxonomic filter parameters
+    valid_levels <- c(
+      "domain",
+      "kingdom",
+      "phylum",
+      "class",
+      "order",
+      "family",
+      "tribe",
+      "genus",
+      "species",
+      "varietas"
     )
 
-    if (isTRUE(filter)) {
-      stopifnot(
-        "Your level parameter must be one of:
-      'domain',
-      'kingdom',
-      'phylum',
-      'class',
-      'order',
-      'family',
-      'tribe',
-      'genus',
-      'species',
-      'varietas'
-      " = level %in%
-          c(
-            "domain",
-            "kingdom",
-            "phylum",
-            "class",
-            "order",
-            "family",
-            "tribe",
-            "genus",
-            "species",
-            "varietas"
-          )
+    if (!level %in% valid_levels) {
+      stop(
+        "level must be one of: ",
+        paste(valid_levels, collapse = ", "),
+        ". Got: ",
+        level
       )
     }
 
-    logger::log_trace("Loading and concatenating prepared libraries")
-    libraries <- files |>
-      purrr::map(
-        .f = tidytable::fread,
-        na.strings = c("", "NA"),
-        colClasses = "character"
-      )
-
-    tables <- libraries |>
-      tidytable::bind_rows() |>
-      split_tables_sop(cache = cache)
-
-    logger::log_trace("Keeping keys")
-    table_keys <- tables$key |>
-      data.frame()
-
-    logger::log_trace("Keeping organisms")
-    table_org_tax_ott <- tables$org_tax_ott
-
-    logger::log_trace("Completing organisms taxonomy")
-    table_org_tax_ott_2 <- table_keys |>
-      tidytable::anti_join(table_org_tax_ott) |>
-      tidytable::distinct(organism = organism_name) |>
-      data.frame()
-
-    if (nrow(table_org_tax_ott_2) != 0) {
-      table_org_tax_ott_full <-
-        table_org_tax_ott_2 |>
-        get_organism_taxonomy_ott(retry = FALSE)
-
-      table_org_tax_ott <-
-        table_org_tax_ott |>
-        tidytable::bind_rows(
-          table_org_tax_ott_full |>
-            tidytable::as_tidytable() |>
-            tidytable::mutate(tidytable::across(
-              .cols = tidyselect::where(is.numeric),
-              .fns = as.character
-            )) |>
-            tidytable::mutate(tidytable::across(
-              .cols = tidyselect::where(is.list),
-              .fns = as.character
-            )) |>
-            tidytable::mutate(tidytable::across(
-              .cols = tidyselect::where(is.logical),
-              .fns = as.character
-            ))
-        )
+    if (!is.character(value) || length(value) != 1L) {
+      stop("value must be a single character string")
     }
+  }
 
-    logger::log_trace("Keeping structures")
-    table_structures_stereo <- tables$str_stereo
-    table_structures_metadata <- tables$str_met
-    table_structures_names <- tables$str_nam
-    table_structures_taxonomy_cla <- tables$str_tax_cla
-    table_structures_taxonomy_npc <- tables$str_tax_npc
+  logger::log_info("Filtering by ", level, ": ", value)
 
-    ## ISSUE see #19
-    # logger::log_trace("Completing structures names")
-    # logger::log_trace("Completing structures taxonomy (classyfire)")
-    # logger::log_trace("Completing structures taxonomy (NPC)")
+  logger::log_trace("Loading and concatenating prepared libraries")
+  libraries <- files |>
+    purrr::map(
+      .f = tidytable::fread,
+      na.strings = c("", "NA"),
+      colClasses = "character"
+    )
 
-    ## If filter is TRUE,
-    ## filter the library based on the specified level and value
-    if (filter == TRUE) {
-      logger::log_trace("Filtering library")
-      table_keys <- table_keys |>
-        tidytable::left_join(table_org_tax_ott)
+  tables <- libraries |>
+    tidytable::bind_rows() |>
+    split_tables_sop(cache = cache)
 
-      table_keys <- table_keys |>
-        tidytable::filter(grepl(
-          x = !!as.name(colnames(table_keys)[grepl(
-            pattern = level,
-            x = colnames(table_keys),
-            perl = TRUE
-          )]),
-          pattern = value,
+  logger::log_trace("Keeping keys")
+  table_keys <- tables$key |>
+    data.frame()
+
+  logger::log_trace("Keeping organisms")
+  table_org_tax_ott <- tables$org_tax_ott
+
+  logger::log_trace("Completing organisms taxonomy")
+  table_org_tax_ott_2 <- table_keys |>
+    tidytable::anti_join(table_org_tax_ott) |>
+    tidytable::distinct(organism = organism_name) |>
+    data.frame()
+
+  if (nrow(table_org_tax_ott_2) != 0) {
+    table_org_tax_ott_full <-
+      table_org_tax_ott_2 |>
+      get_organism_taxonomy_ott(retry = FALSE)
+
+    table_org_tax_ott <-
+      table_org_tax_ott |>
+      tidytable::bind_rows(
+        table_org_tax_ott_full |>
+          tidytable::as_tidytable() |>
+          tidytable::mutate(tidytable::across(
+            .cols = tidyselect::where(is.numeric),
+            .fns = as.character
+          )) |>
+          tidytable::mutate(tidytable::across(
+            .cols = tidyselect::where(is.list),
+            .fns = as.character
+          )) |>
+          tidytable::mutate(tidytable::across(
+            .cols = tidyselect::where(is.logical),
+            .fns = as.character
+          ))
+      )
+  }
+
+  logger::log_trace("Keeping structures")
+  table_structures_stereo <- tables$str_stereo
+  table_structures_metadata <- tables$str_met
+  table_structures_names <- tables$str_nam
+  table_structures_taxonomy_cla <- tables$str_tax_cla
+  table_structures_taxonomy_npc <- tables$str_tax_npc
+
+  ## ISSUE see #19
+  # logger::log_trace("Completing structures names")
+  # logger::log_trace("Completing structures taxonomy (classyfire)")
+  # logger::log_trace("Completing structures taxonomy (NPC)")
+
+  ## If filter is TRUE,
+  ## filter the library based on the specified level and value
+  if (filter == TRUE) {
+    logger::log_trace("Filtering library")
+    table_keys <- table_keys |>
+      tidytable::left_join(table_org_tax_ott)
+
+    table_keys <- table_keys |>
+      tidytable::filter(grepl(
+        x = !!as.name(colnames(table_keys)[grepl(
+          pattern = level,
+          x = colnames(table_keys),
           perl = TRUE
-        )) |>
-        tidytable::select(
-          structure_inchikey,
-          structure_smiles_no_stereo,
-          organism_name,
-          reference_doi
-        ) |>
-        tidytable::distinct()
+        )]),
+        pattern = value,
+        perl = TRUE
+      )) |>
+      tidytable::select(
+        structure_inchikey,
+        structure_smiles_no_stereo,
+        organism_name,
+        reference_doi
+      ) |>
+      tidytable::distinct()
 
-      stopifnot(
-        "Your filter led to no entries,
+    stopifnot(
+      "Your filter led to no entries,
         try to change it." = nrow(table_keys) != 0
-      )
-    }
-
-    export_params(
-      parameters = get_params(step = "prepare_libraries_sop_merged"),
-      step = "prepare_libraries_sop_merged"
-    )
-    export_output(x = table_keys, file = output_key)
-    export_output(x = table_org_tax_ott, file = output_org_tax_ott)
-    export_output(x = table_structures_stereo, file = output_str_stereo)
-    export_output(x = table_structures_metadata, file = output_str_met)
-    export_output(x = table_structures_names, file = output_str_nam)
-    export_output(x = table_structures_taxonomy_cla, file = output_str_tax_cla)
-    export_output(x = table_structures_taxonomy_npc, file = output_str_tax_npc)
-
-    rm(
-      table_keys,
-      table_org_tax_ott,
-      table_structures_stereo,
-      table_structures_metadata,
-      table_structures_names,
-      table_structures_taxonomy_cla,
-      table_structures_taxonomy_npc
-    )
-    return(
-      c(
-        "key" = output_key,
-        "org_tax_ott" = output_org_tax_ott,
-        "str_stereo" = output_str_stereo,
-        "str_met" = output_str_met,
-        "str_name" = output_str_nam,
-        "str_tax_cla" = output_str_tax_cla,
-        "str_tax_npc" = output_str_tax_npc
-      )
     )
   }
+
+  export_params(
+    parameters = get_params(step = "prepare_libraries_sop_merged"),
+    step = "prepare_libraries_sop_merged"
+  )
+  export_output(x = table_keys, file = output_key)
+  export_output(x = table_org_tax_ott, file = output_org_tax_ott)
+  export_output(x = table_structures_stereo, file = output_str_stereo)
+  export_output(x = table_structures_metadata, file = output_str_met)
+  export_output(x = table_structures_names, file = output_str_nam)
+  export_output(x = table_structures_taxonomy_cla, file = output_str_tax_cla)
+  export_output(x = table_structures_taxonomy_npc, file = output_str_tax_npc)
+
+  rm(
+    table_keys,
+    table_org_tax_ott,
+    table_structures_stereo,
+    table_structures_metadata,
+    table_structures_names,
+    table_structures_taxonomy_cla,
+    table_structures_taxonomy_npc
+  )
+  return(
+    c(
+      "key" = output_key,
+      "org_tax_ott" = output_org_tax_ott,
+      "str_stereo" = output_str_stereo,
+      "str_met" = output_str_met,
+      "str_name" = output_str_nam,
+      "str_tax_cla" = output_str_tax_cla,
+      "str_tax_npc" = output_str_tax_npc
+    )
+  )
+}
