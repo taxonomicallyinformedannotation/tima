3491d599b889867d0a1f5db784ddcbaed1b2c805
Adriano Rutz <adriano.rutz@hotmail.com>
2025-11-04 21:34:22 +0100

    feat(auth): update codemeta.json to reflect R version 4.5.2

--- R/c_wrappers.R
+++ R/c_wrappers.R
@@ -43,8 +43,8 @@ gnps_wrapper <- function(x, y) {
 #' @param ppm Numeric value specifying the relative tolerance in ppm
 #'
 #' @return A list with two integer vectors:
-#'   \item{[[1]]}{Indices of matched peaks in x}
-#'   \item{[[2]]}{Indices of matched peaks in y}
+#'   \item{1}{Indices of matched peaks in x}
+#'   \item{2}{Indices of matched peaks in y}
 #'
 #' @export
 #'
