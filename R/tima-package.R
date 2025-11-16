be793e4134ffc0781cad69369f28f5aae6cf5ba6
Adriano Rutz <adriano.rutz@hotmail.com>
2025-11-16 01:09:07 +0100

    fix

--- R/tima-package.R
+++ R/tima-package.R
@@ -9,14 +9,19 @@ NULL
 .datatable.aware <- TRUE
 
 .onLoad <- function(libname, pkgname) {
-  ## Hack to avoid rcmdcheck warning
+  # Initialize logging from environment (safe to call multiple times)
+  try({ init_logging() }, silent = TRUE)
+
+  # Hints/operators to appease R CMD check and lazy loading quirks
   DT::`%>%`
   gt::`%>%`
   shinybusy::use_busy_spinner()
   shinyWidgets::animations
   visNetwork::`%>%`
-  reticulate::py_require(packages = "rdkit")
-  setup_logger(filename = paste0(pkgname, ".log"))
+
+  # Ensure RDKit availability for Python-based features (no-op if unavailable)
+  try({ reticulate::py_require(packages = "rdkit") }, silent = TRUE)
+
   invisible()
 }
 
@@ -26,5 +31,4 @@ NULL
     utils::citation(package = pkgname) |>
       format()
   )
-  setup_logger(filename = paste0(pkgname, ".log"))
 }
