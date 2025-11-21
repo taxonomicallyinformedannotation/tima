7e9f6c843c703cbe204f31d1c9cc2d2f13d26176
Adriano Rutz <adriano.rutz@hotmail.com>
2025-11-21 12:33:42 +0100

    tests

--- tests/testthat/test-parse_yaml_params.R
+++ tests/testthat/test-parse_yaml_params.R
@@ -3,20 +3,23 @@
 library(testthat)
 
 test_that("parse_yaml_params errors if default file missing", {
-  expect_error(parse_yaml_params(def = "missing.yaml", usr = NULL), "not found")
+  expect_error(parse_yaml_params(def = temp_test_path("missing.yaml"), usr = NULL), "not found")
 })
 
 test_that("parse_yaml_params loads default YAML", {
-  writeLines("a: 1", "def.yaml")
-  params <- parse_yaml_params(def = "def.yaml", usr = NULL)
+  def_file <- temp_test_path("def.yaml")
+  writeLines("a: 1", def_file)
+  params <- parse_yaml_params(def = def_file, usr = NULL)
   expect_type(params, "list")
   expect_equal(params$a, 1)
 })
 
 test_that("parse_yaml_params merges user YAML overriding defaults", {
-  writeLines("a: 1\nb: 2", "def.yaml")
-  writeLines("b: 3\nc: 4", "usr.yaml")
-  params <- parse_yaml_params(def = "def.yaml", usr = "usr.yaml")
+  def_file <- temp_test_path("def.yaml")
+  usr_file <- temp_test_path("usr.yaml")
+  writeLines("a: 1\nb: 2", def_file)
+  writeLines("b: 3\nc: 4", usr_file)
+  params <- parse_yaml_params(def = def_file, usr = usr_file)
   expect_equal(params$b, 3)
   expect_equal(params$a, 1)
   expect_equal(params$c, 4)
